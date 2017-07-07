6c73d0c311947325aaf72dd15cf408deea67b96a
starday <starday01@yahoo.com>
2017-07-07 13:03:44 +0100

    final push on friday 7/7/2017

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FacilitiesService, PersonService, CorporateFacilityService } from '../services/facility-manager/setup/index';
 
 @Component({
   selector: 'app-signup',
@@ -14,7 +15,16 @@ export class SignupComponent implements OnInit {
   pwdReset_on = false;
   login_on = false;
 
-  constructor() { }
+  constructor(private corporateFacilityService: CorporateFacilityService, private facilityService: FacilitiesService) {
+    // this.corporateFacilityService.listenerUpdate.subscribe(value => {
+    //   console.log(value);
+    //   this.facilityService.announceNotification({
+    //     type: 'Success',
+    //     text: 'Welcome ' + value.name,
+    //     users: [this.facilityService.getLoginUserId()]
+    //   })
+    // })
+  }
 
   ngOnInit() {
   }
