148d398f31d06108e5e8a9960aa206cb640f65c7
chisimdi <ezeanietochisimdi@gmail.com>
2017-07-22 18:36:07 +0100

    Updated

--- src/app/system-modules/module-menu/pharmacy/dispense/prescription/prescription.component.ts
+++ src/app/system-modules/module-menu/pharmacy/dispense/prescription/prescription.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, Output, Input } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CoolSessionStorage } from 'angular2-cool-storage';
-import { Facility, Prescription, PrescriptionItem, Dispense, Inventory, InventoryTransaction, User,
+import { Facility, Prescription, PrescriptionItem, Dispense, Inventory, InventoryTransaction, User, 
+	Dispensed, DispensedArray, BatchTransaction,
 	DispenseByPrescription, DispenseByNoprescription, DispenseItem, MedicationList, BillIGroup, BillItem } from '../../../../../models/index';
 import { Clients } from '../../../../../shared-module/helpers/global-config';
 import { PharmacyEmitterService } from '../../../../../services/facility-manager/pharmacy-emitter.service';
-import { FacilitiesService, PrescriptionService,
+import { FacilitiesService, PrescriptionService, InventoryTransactionTypeService,
 	DispenseService, MedicationListService, InventoryService, BillingService} from '../../../../../services/facility-manager/setup/index';
 
 @Component({
@@ -17,7 +18,6 @@ import { FacilitiesService, PrescriptionService,
 export class PrescriptionComponent implements OnInit {
 	@Output() prescriptionItems: Prescription = <Prescription>{};
 	@Input() employeeDetails: any;
-	//dispenseForm: FormGroup;
 	facility: Facility = <Facility>{};
 	user: any = <any>{};
 	selectedPrescription: PrescriptionItem = <PrescriptionItem>{};
@@ -28,12 +28,15 @@ export class PrescriptionComponent implements OnInit {
 	transactions: Inventory = <Inventory>{};
 	viewTransactions: InventoryTransaction[] = [];
 	storeId: any = {};
-	//totalCostOnPre = 0;
 	totalQuantity = 0;
-	//totalQtyOnPre = 0;
 	totalCost = 0;
 	loading = true;
-	batchLoading = true;
+	batchLoading: boolean = true;
+	disableDispenseBtn: Boolean = false;
+	qtyDispenseBtn: string = 'Dispense';
+	inventoryTransactionTypeId: string = '';
+	disablePaymentBtn: boolean = false;
+	paymentStatusText: string = '<i class="fa fa-refresh"></i> Refresh Payment Status';
 
 	constructor(
 		//private _fb: FormBuilder,
@@ -46,7 +49,8 @@ export class PrescriptionComponent implements OnInit {
 		private _dispenseService: DispenseService,
 		private _inventoryService: InventoryService,
 		//private _medicationListService: MedicationListService,
-		private _billingService: BillingService
+		private _billingService: BillingService,
+		private _inventoryTransactionTypeService: InventoryTransactionTypeService
 	) {
 
 	}
@@ -61,13 +65,12 @@ export class PrescriptionComponent implements OnInit {
 			this.prescriptionId = params['id'];
 		});
 
-		this.getPrescriptionDetails();
+		this._getPrescriptionDetails();
+		this._getInventoryTransactionTypes();
 	}
 
 	// Save prescription
 	onClickSavePrescription() {
-		console.log(this.prescriptionItems);
-		console.log(this.unBilledArray);
 		const tempArray = [];
 		// Get the billed items from the unbilled items
 		this.unBilledArray.forEach((element, i) => {
@@ -305,7 +308,7 @@ export class PrescriptionComponent implements OnInit {
 	}
 
 	// Get all drugs from generic
-	getPrescriptionDetails() {
+	private _getPrescriptionDetails() {
 		this._prescriptionService.get(this.prescriptionId, {})
 			.then(res => {
 				console.log(res);
@@ -336,21 +339,8 @@ export class PrescriptionComponent implements OnInit {
 				});
 
 				if(this.prescriptions.length > 0) {
-					if(this.prescriptionItems.billId !== undefined) {
-						this._billingService.get(this.prescriptionItems.billId, {})
-							.then(res => {
-								if(res._id !== undefined) {
-									res.billItems.forEach(i => {
-										this.prescriptions.forEach(j => {
-											if(i.serviceId === j.serviceId) {
-												j.paymentCompleted = i.paymentCompleted;
-											}
-										});
-									});
-								}
-							})
-							.catch(err => { console.log(err); });
-					}
+					console.log('Calling');
+					this._getPaymentStatus();
 				}
 			})
 			.catch(err => {
@@ -359,8 +349,6 @@ export class PrescriptionComponent implements OnInit {
 	}
 
 	onClickEachPrescription(index, prescription) {
-		console.log(index);
-		console.log(prescription);
 		if(prescription.isBilled) {
 			this.selectedPrescription = prescription;
 			this.selectedPrescription.isOpen = !this.selectedPrescription.isOpen;
@@ -408,94 +396,166 @@ export class PrescriptionComponent implements OnInit {
 	onClickBillProduct(parentIndex, index, batch, inputBatch) {
 		const item = this.prescriptionItems.prescriptionItems.filter(e => e._id === this.prescriptions[parentIndex]._id);
 		const itemId = item[0]._id;
-		// Check if the qty entered is less than or equal to the qty needed.
-		if(inputBatch[index] <= item[0].quantity) {
-			this.transactions.transactions.forEach(element => {	
-				if(element._id === batch._id) {
-					element.quantity = element.quantity - inputBatch[index];
-				}
-			});
-			this.transactions.totalQuantity = this.transactions.totalQuantity - inputBatch[index];
-
-			if(this.storeId.typeObject.storeId !== undefined) {
-				// Update the quantityDispensed in the selected item.
-				const itemIndex = this.prescriptionItems.prescriptionItems.findIndex(item => item._id == itemId);
-				this.prescriptionItems.prescriptionItems[itemIndex].quantityDispensed = inputBatch[index];
-
-				// Make a call to update the prescription with the qty dispensed
-				this._prescriptionService.update(this.prescriptionItems)
-					.then(res => {
-						console.log(res);
-						if(res._id !== undefined) {
-							// Make a call to the inventory service so that you can deduct the quantity from the inventory
-							this._inventoryService.patch(this.transactions._id, this.transactions, {})
-								.then(res => {
-									console.log(res);
-									this._notification('Success', 'Quantity has been deducted.');
-								})
-								.catch(err => {
-									console.log(err);
-								});
-						}
-					})
-					.catch(err => {
-						console.log(err);
-					});
-			} else {
-				this._notification('Info', 'Please check into store!');
-			}
+		// Input validation
+		if(inputBatch[index] <= 0 || inputBatch[index] === '' || isNaN(inputBatch[index]) ) {
+			this._notification('Info', 'Please enter a valid number greater than 0');
 		} else {
-			this._notification('Info', 'The quantity entered is greater than the quantity requested!');
+			// Check if the qty entered is less than or equal to the qty needed.
+			if(inputBatch[index] <= item[0].quantity) {
+				// Check if the qty entered plus the quantity dispensed already,
+				// if it's less than or equal to the qty needed to dispense.
+				let qtyPlusQtyToDispense = item[0].quantityDispensed + inputBatch[index];
+				if((item[0].quantity - qtyPlusQtyToDispense) >= 0) {
+					if(this.storeId.typeObject.storeId !== undefined) {
+						// disable the dispense button.
+						this.disableDispenseBtn = true;
+						this.qtyDispenseBtn = "Dispensing... <i class='fa fa-spinner fa-spin'></i>";
+						// Update the quantityDispensed in the selected item.
+						const itemIndex = this.prescriptionItems.prescriptionItems.findIndex(item => item._id == itemId);
+						this.prescriptionItems.prescriptionItems[itemIndex].quantityDispensed += inputBatch[index];
+						// Build the dispense client model
+						this._dispensedBatchTracking(itemIndex, inputBatch[index], batch.batchNumber);
+
+						// Make a call to update the prescription with the qty dispensed
+						this._prescriptionService.update(this.prescriptionItems)
+							.then(res => {
+								console.log(res);
+								if(res._id !== undefined) {
+									this._batchTransactionTracking(index, inputBatch[index], batch);
+									// // Deduct from the batches before updating the batches in the inventory.
+									// this.transactions.transactions.forEach(element => {
+									// 	if(element._id === batch._id) {
+									// 		element.quantity = element.quantity - inputBatch[index];
+									// 	}
+									// });
+									// this.transactions.totalQuantity = this.transactions.totalQuantity - inputBatch[index];
+									// Make a call to the inventory service so that you can deduct the quantity from the inventory
+									this._inventoryService.patch(this.transactions._id, this.transactions, {})
+										.then(res => {
+											// disable the dispense button.
+											this.disableDispenseBtn = false;
+											this.qtyDispenseBtn = "Dispense";
+											console.log(res);
+											this._notification('Success', 'Quantity has been deducted.');
+										})
+										.catch(err => {
+											console.log(err);
+										});
+								}
+							})
+							.catch(err => {
+								console.log(err);
+							});
+					} else {
+						this._notification('Info', 'Please check into store!');
+					}
+				} else {
+					this._notification('Info', 'The quantity entered is greater than the quantity requested!');
+				}
+			} else {
+				this._notification('Info', 'The quantity entered is greater than the quantity requested!');
+			}
 		}
-		
 	}
-
-	// Send bill to billing service to bill the patient
-	// onClickBillPatient() {
-	// 	console.log(this.prescriptionItems);
-	// 	const billItemArray = []
-	// 	this.prescriptionItems.prescriptionItems.forEach(element => {
-	// 		const billItem = <BillItem> {
-	// 			facilityServiceId: element.facilityServiceId,
-	// 			serviceId: element.serviceId,
-	// 			facilityId: this.facility._id,
-	// 			patientId: this.prescriptionItems.patientId,
-	// 			quantity: element.quantity,
-	// 			totalPrice: element.totalCost,
-	// 			unitPrice: element.cost,
-	// 			unitDiscountedAmount: 0,
-  	// 			totalDiscoutedAmount: 0,
-	// 		};
-
-	// 		billItemArray.push(billItem);
-	// 	});
-
-	// 	const bill = <BillIGroup> {
-	// 		facilityId: this.facility._id,
-	// 		patientId: this.prescriptionItems.patientId,
-	// 		billItems: billItemArray,
-	// 		discount: 0,
-	// 		subTotal: this.totalCost,
-	// 		grandTotal: this.totalCost,
-	// 	}
-	// 	this._billingService.create(bill)
-	// 		.then(res => {
-	// 			console.log(res);
-	// 		})
-	// 		.catch(err => {
-	// 			console.log(err);
-	// 		});
-	// }
 
 	billToggle() {
 		this.billshow = !this.billshow;
 	}
 
+	onClickRefreshPaymentStatus() {
+		this._getPaymentStatus();
+	}
+
+	// Dispense resolver
+	private _dispensedBatchTracking(index:number, qty:number, bNumber:string): void {
+		const dispensedKey = this.prescriptionItems.prescriptionItems[index];
+		let orderIndex: number = 0;
+
+		if(dispensedKey.dispensed.dispensedArray === undefined) {
+			orderIndex = orderIndex;
+		} else {
+			orderIndex = dispensedKey.dispensed.dispensedArray.length;
+		}
+
+		dispensedKey.dispensed.totalQtyDispensed = dispensedKey.quantityDispensed;
+		dispensedKey.dispensed.outstandingBalance = dispensedKey.quantity - dispensedKey.quantityDispensed;
+
+		let item: DispensedArray = {
+			orderIndex: orderIndex, // unique
+			dispensedDate: new Date,
+			batchNumber: bNumber,
+			qty: qty,
+			employeeName: this.prescriptionItems.employeeName,
+			storeName: this.storeId.typeObject.storeObject.name,
+			unitBilledPrice: dispensedKey.cost,
+			totalAmount: dispensedKey.cost * qty
+		};
+
+		dispensedKey.dispensed.dispensedArray.push(item);
+	}
+
+	private _batchTransactionTracking(index: number, qty: number, batch: any) {
+		// Deduct from the batches before updating the batches in the inventory.
+		this.transactions.transactions.forEach(element => {
+			if(element._id === batch._id) {
+				let batchTransaction: BatchTransaction = {
+					batchNumber: batch.batchNumber,
+					employeeId: this.prescriptionItems.employeeId,
+					employeeName: this.prescriptionItems.employeeName,
+					preQuantity: batch.quantity, // Before Operation.
+					postQuantity: batch.quantity - qty, // After Operation.
+					quantity: qty, // Operational qty.
+					referenceId: this.prescriptionItems._id, // Dispense id, Transfer id...
+					referenceService: 'Prescription/Dispense Service', // Dispense, Transfer...
+					inventorytransactionTypeId: this.inventoryTransactionTypeId,
+				}
+				element.batchTransactions.push(batchTransaction);
+				element.quantity = element.quantity - qty;
+			}
+		});
+		this.transactions.totalQuantity = this.transactions.totalQuantity - qty;
+	}
+
+	// Get payment status
+	private _getPaymentStatus() {
+		this.disablePaymentBtn = true;
+		this.paymentStatusText = 'Getting Payment Status... <i class="fa fa-spinner fa-spin"></i>';
+		if(this.prescriptionItems.billId !== undefined) {
+			this._billingService.get(this.prescriptionItems.billId, {})
+				.then(res => {
+					if(res._id !== undefined) {
+						this.disablePaymentBtn = false;
+						res.billItems.forEach(i => {
+							this.prescriptions.forEach(j => {
+								if(i.serviceId === j.serviceId) {
+									j.paymentCompleted = i.paymentCompleted;
+								}
+							});
+						});
+					}
+				})
+				.catch(err => { console.log(err); });
+		}
+	}
+
+	// Notification
 	private _notification(type: string, text: string): void {
 		this._facilityService.announceNotification({
 			users: [this.user._id],
 			type: type,
 			text: text
 		});
+	}
+
+	// Get all the inventory transaction types.
+	private _getInventoryTransactionTypes() {
+		this._inventoryTransactionTypeService.findAll()
+			.then(res => {
+				if(res.data.length > 0) {
+					const inventoryType = res.data.filter(x => x.name.toLowerCase().includes('dispense'));
+					this.inventoryTransactionTypeId = inventoryType[0]._id;
+				}
+			})
+			.catch(err => { console.log(err); });
 	}
 }
