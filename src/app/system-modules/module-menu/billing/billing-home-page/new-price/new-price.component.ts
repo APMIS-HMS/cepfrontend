b7cd543d3b51f60c8f62d44eb40395e947d010ab
subair adams <subaru4ten@gmail.com>
2017-08-01 16:21:25 +0100

    --updated

--- src/app/system-modules/module-menu/billing/billing-home-page/new-price/new-price.component.ts
+++ src/app/system-modules/module-menu/billing/billing-home-page/new-price/new-price.component.ts
@@ -22,6 +22,13 @@ export class NewPriceComponent implements OnInit {
   facility: Facility = <Facility>{};
   public frmNewprice: FormGroup;
 
+  get service(): FormArray {
+    return <FormArray>this.frmNewprice.get('service');
+  }
+  get price(): FormArray {
+    return <FormArray>this.frmNewprice.get('price');
+  }
+  
   constructor(private formBuilder: FormBuilder, private _facilitiesServiceCategoryService: FacilitiesServiceCategoryService,
     private _locker: CoolSessionStorage, private servicePriceService: ServicePriceService) {
   }
@@ -40,12 +47,13 @@ export class NewPriceComponent implements OnInit {
   addNew() {
     this.frmNewprice = this.formBuilder.group({
       serviceCat: ['', [<any>Validators.required]],
-      service: ['', [<any>Validators.required]],
-     // service: new FormArray ([], <any>Validators.required),
-      price: [0.00, [<any>Validators.required]]
-      //price:new FormArray([], <any>Validators.required )
+     // service: ['', [<any>Validators.required]],
+     service: this.formBuilder.array(['service'],<any>Validators.required),
+     price: this.formBuilder.array([],<any>Validators.required)
+     //price: [0.00, [<any>Validators.required]]
     });
   }
+
   filterServices(itemj) {
     this.services = [];
     itemj.services.forEach((itemk, k) => {
