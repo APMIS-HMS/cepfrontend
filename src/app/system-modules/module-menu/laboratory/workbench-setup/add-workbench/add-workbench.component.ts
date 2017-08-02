e945caa8ad0a3c7d37ecb1d5f850b28ecfb88393
chisimdi <ezeanietochisimdi@gmail.com>
2017-08-02 17:25:46 +0100

    Updated

--- src/app/system-modules/module-menu/laboratory/workbench-setup/add-workbench/add-workbench.component.ts
+++ src/app/system-modules/module-menu/laboratory/workbench-setup/add-workbench/add-workbench.component.ts
@@ -4,7 +4,7 @@ import { CoolSessionStorage } from 'angular2-cool-storage';
 import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Facility, User } from '../../../../../models/index';
 import {
-    FacilitiesService
+    FacilitiesService, WorkbenchService, LaboratoryService
 } from '../../../../../services/facility-manager/setup/index';
 import { Subject } from 'rxjs/Subject';
 
@@ -16,11 +16,17 @@ import { Subject } from 'rxjs/Subject';
 export class AddWorkbenchComponent implements OnInit {
   addWorkbench: FormGroup;
   facility: Facility = <Facility>{};
-	user: User = <User>{};
+  user: User = <User>{};
+  laboratories: any[] = [];
+  workbenchBtnText: string = 'Add Workbench';
+  disableWorkbenchBtn: boolean = false;
 
   constructor(
     private _fb: FormBuilder,
     private _locker: CoolSessionStorage,
+    private _facilityService: FacilitiesService,
+    private _workbenchService: WorkbenchService,
+    private _laboratoryService: LaboratoryService
   ) { }
 
   ngOnInit() {
@@ -32,5 +38,47 @@ export class AddWorkbenchComponent implements OnInit {
       workbenchName: ['', [<any>Validators.required]]
     });
   }
+
+  saveWorkbench(value: any, valid: boolean): void {
+    if(valid) {
+      this.disableWorkbenchBtn = true;
+      this.workbenchBtnText = "Processing... <i class='fa fa-spinner fa-spin'></i>";
+      console.log(value);
+      console.log(valid);
+
+      const workbench = ({
+        workbench: value.workbenchName,
+        labName: value.labName
+      });
+      console.log(workbench);
+      // Save Workbench
+      this._workbenchService.create(workbench)
+        .then(res => {
+          console.log(res);
+          this.disableWorkbenchBtn = false;
+          this.workbenchBtnText = "Add Workbench";
+        })
+        .catch(err => { console.log(err); });
+    } else {
+      this._notification('Info', 'Some fields are empty. Please ensure that you fill in all fields.');
+    }
+  }
+
+  private _getLaboratories(): void {
+    this._laboratoryService.findAll()
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => { console.log(err); });
+  }
+
+  // Notification
+	private _notification(type: string, text: string): void {
+		this._facilityService.announceNotification({
+			users: [this.user._id],
+			type: type,
+			text: text
+		});
+	}
 
 }
