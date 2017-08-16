977096a35b43dff0fd96c7410e5060b5f138a0e2
sabaoth <sunday@apmis.ng>
2017-08-16 16:02:26 +0100

    updates

--- src/app/services/facility-manager/setup/appointment.service.ts
+++ src/app/services/facility-manager/setup/appointment.service.ts
@@ -91,11 +91,11 @@ export class AppointmentService {
     return this._socket.patch(_id, data, param);
   }
 
-  setMeeting(topic: string, startTime: Date) {
+  setMeeting(topic: string, startTime: Date, appointmentId) {
     const host = this._restService.getHost();
     const path = host + '/zoom-meeting';
     return request
       .post(path)
-      .send({ topic: topic, startTime: startTime }); // query string 
+      .send({ topic: topic, startTime: startTime, appointmentId: appointmentId }); // query string 
   }
 }
