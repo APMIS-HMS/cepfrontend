4a52cba2bb6fa73813454dd36093d27033780155
aiyk <aiyk.ekwe@gmail.com>
2017-08-24 11:09:51 +0100

    cash payment done without paystack fix to bring back actions tbl col

--- src/app/system-modules/module-menu/payment/payment.component.ts
+++ src/app/system-modules/module-menu/payment/payment.component.ts
@@ -16,19 +16,21 @@ export class PaymentComponent implements OnInit {
     selectedBillItem: BillItem = <BillItem>{};
     invoice: Invoice = <Invoice>{ billingDetails: [], totalPrice: 0, totalDiscount: 0 };
 
-    public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    public barChartType:string = 'bar';
-    public barChartLegend:boolean = true;
-    
-    public barChartData:any[] = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-    ];
+    searchInvestigation: FormControl;
 
-    // PolarArea
-    public polarAreaChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
-    public polarAreaChartData:number[] = [300, 500, 100, 40, 120];
-    public polarAreaLegend:boolean = true;
+    // public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    // public barChartType:string = 'bar';
+    // public barChartLegend:boolean = true;
+    
+    // public barChartData:any[] = [
+    //     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    //     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    // ];
+
+    // // PolarArea
+    // public polarAreaChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+    // public polarAreaChartData:number[] = [300, 500, 100, 40, 120];
+    // public polarAreaLegend:boolean = true;
     
     public polarAreaChartType:string = 'polarArea';
 
@@ -39,42 +41,12 @@ export class PaymentComponent implements OnInit {
         private patientService: PatientService) {
     }
     ngOnInit() {
+        this.searchInvestigation = new FormControl('', []);
     }
 
-    public barChartOptions:any = {
-        scaleShowVerticalLines: false,
-        responsive: true
-    };
+    // public barChartOptions:any = {
+    //     scaleShowVerticalLines: false,
+    //     responsive: true
+    // };
     
-    // events
-    public chartClicked(e:any):void {
-        console.log(e);
-    }
-    
-    public chartHovered(e:any):void {
-        console.log(e);
-    }
-    
-    public randomize():void {
-        // Only Change 3 values
-        let data = [
-        Math.round(Math.random() * 100),
-        59,
-        80,
-        (Math.random() * 100),
-        56,
-        (Math.random() * 100),
-        40];
-        let clone = JSON.parse(JSON.stringify(this.barChartData));
-        clone[0].data = data;
-        this.barChartData = clone;
-        /**
-         * (My guess), for Angular to recognize the change in the dataset
-         * it has to change the dataset variable directly,
-         * so one way around it, is to clone the data, change it and then
-         * assign it;
-         */
-    }
-    
-
 }
