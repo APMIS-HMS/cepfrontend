11345bd5ce20fd2c0a0f1fbb610b96f740ae4900
subair adams <finestadams@gmail.com>
2017-08-25 17:57:23 +0100

    --updated

--- src/app/system-modules/module-menu/inventory-manager/inventory-manager.routes.ts
+++ src/app/system-modules/module-menu/inventory-manager/inventory-manager.routes.ts
@@ -6,7 +6,9 @@ import { StockHistoryComponent } from './stock-history/stock-history.component';
 import { ReceiveStockComponent } from './receive-stock/receive-stock.component';
 import { RequisitionComponent } from './requisition/requisition.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import {InitializeStoreComponent} from './initialize-store/initialize-store.component'
 import { LoginEmployeeResolverService } from '../../../resolvers/module-menu/index';
+
 
 const INVENTORYMODULES_ROUTES: Routes = [
     {
@@ -16,6 +18,7 @@ const INVENTORYMODULES_ROUTES: Routes = [
                 path: 'inventory', component: LandingpageComponent,
                 // resolve: { loginEmployee: LoginEmployeeResolverService }
             },
+            { path: 'initialize-store', component: InitializeStoreComponent },
             { path: 'stock-taking', component: StockTakingComponent },
             { path: 'stock-transfer', component: StockTransferComponent },
             { path: 'stock-history', component: StockHistoryComponent },
