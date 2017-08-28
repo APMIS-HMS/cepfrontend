217c8a2611006c778e994060d275513f31693f7f
sabaoth <sunday@apmis.ng>
2017-08-28 14:09:12 +0100

    updates

---
+++ src/app/system-modules/module-menu/health-coverage/family-cover/fc-beneficiary-list/fc-beneficiary-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fc-beneficiary-list',
+  templateUrl: './fc-beneficiary-list.component.html',
+  styleUrls: ['./fc-beneficiary-list.component.scss']
+})
+export class FcBeneficiaryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
