2f1bb0256bd61bac96f5528922028b48a5273044
sabaoth <sunday@apmis.ng>
2017-09-07 15:23:56 +0100

    updates@starday

--- src/app/system-modules/module-menu/forms-manager/forms-manager.component.ts
+++ src/app/system-modules/module-menu/forms-manager/forms-manager.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Router, Event, NavigationStart, ActivatedRoute, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-forms-manager',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsManagerComponent implements OnInit {
 
-  constructor() { }
+  pageInView = 'Treatement Template Manager';
+  
+  treatementTemplate = true;
+  formGen = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    const page: string = this.router.url;
+    this.checkPageUrl(page);
   }
+
+  treatementTemplate_show() {
+    this.treatementTemplate = true;
+    this.formGen = false;
+    this.pageInView = "Treatement Template Manager";
+    this.router.navigate(['/dashboard/forms-manager/treatement-template']);
+  }
+  formGen_show() {
+    this.treatementTemplate = false;
+    this.formGen = true;
+    this.pageInView = "Form Generator";
+    this.router.navigate(['/dashboard/forms-manager/forms']);
+  }
+  private checkPageUrl(param: string) {
+		if (param.includes('treatement-template')) {
+			this.treatementTemplate = true;
+      this.formGen = false;
+      this.pageInView = "Treatement Template Manager";
+		} else if (param.includes('forms')) {
+			this.treatementTemplate = false;
+      this.formGen = true;
+      this.pageInView = "Form Generator";
+		} 
+	}
 
 }
