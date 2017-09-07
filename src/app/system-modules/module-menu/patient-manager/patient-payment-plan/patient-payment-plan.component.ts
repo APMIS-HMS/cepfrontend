4af40a3f02d6a47cb67bfc3aeb70cc263f15bb85
aiyk <aiyk.ekwe@gmail.com>
2017-09-07 09:39:22 +0100

    patient payment redesign

--- src/app/system-modules/module-menu/patient-manager/patient-payment-plan/patient-payment-plan.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patient-payment-plan/patient-payment-plan.component.ts
@@ -10,6 +10,11 @@ export class PatientPaymentPlanComponent implements OnInit {
 
   mainErr = true;
   errMsg = 'you have unresolved errors';
+
+  tabWallet = true;
+  tabInsurance = false;
+  tabCompany = false;
+  tabFamily = false;
   
   walletPlan = new FormControl('', Validators.required);
   walletPlanCheck = new FormControl('');
@@ -27,6 +32,31 @@ export class PatientPaymentPlanComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  tabWallet_click() {
+    this.tabWallet = true;
+    this.tabCompany = false;
+    this.tabFamily = false;
+    this.tabInsurance = false;
+  }
+  tabCompany_click() {
+    this.tabWallet = false;
+    this.tabCompany = true;
+    this.tabFamily = false;
+    this.tabInsurance = false;
+  }
+  tabFamily_click() {
+    this.tabWallet = false;
+    this.tabCompany = false;
+    this.tabFamily = true;
+    this.tabInsurance = false;
+  }
+  tabInsurance_click() {
+    this.tabWallet = false;
+    this.tabCompany = false;
+    this.tabFamily = false;
+    this.tabInsurance = true;
   }
 
   close_onClick() {
