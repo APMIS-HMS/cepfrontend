7228779307956a1d4f991d8aa0de59ceb11db779
sabaoth <sunday@apmis.ng>
2017-09-07 20:20:08 +0100

    updates@starday

--- src/app/system-modules/module-menu/patient-manager/patient-payment-plan/patient-payment-plan.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patient-payment-plan/patient-payment-plan.component.ts
@@ -38,6 +38,8 @@ export class PatientPaymentPlanComponent implements OnInit {
   plans: any[] = [];
   user: User = <User>{};
   insurancePlanForm: FormGroup;
+
+  insuranceFormArrayIndex = 0;
   constructor(private formBuilder: FormBuilder, private hmoService: HmoService, private locker: CoolSessionStorage,
     private facilityService: FacilitiesService) { }
 
@@ -51,35 +53,7 @@ export class PatientPaymentPlanComponent implements OnInit {
       }
     });
     this.addInsurancePlan();
-    (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[0]).controls['hmo'].valueChanges.subscribe(value => {
-      this.selectedHMO = value;
-      this.hmoService.hmos(this.selectedFacility._id, value).then(payload => {
-        console.log(payload.body);
-        this.plans = payload.body;
-        (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[0]).controls['hmoPlan'].setValue(this.plans);
-      });
-    });
-    (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[0]).controls['hmoPlanId'].valueChanges
-      .debounceTime(300)
-      .distinctUntilChanged()
-      .subscribe(value => {
-        console.log(value);
-        this.selectedHMO =  (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[0]).controls['hmo'].value;
-        if (this.selectedHMO === undefined) {
-          this._notification('Warning', 'Please select an HMO to search from and try again!');
-        } else {
-          this.hmoService.hmos(this.selectedFacility._id, this.selectedHMO, value).then(payload => {
-            console.log(payload);
-            if (payload.body !== null) {
-              this.selectedHMOClient = payload.body;
-              this.selectedHMO =  (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[0]).controls['hmoPlan'].setValue(payload.body.plan);
-            } else {
-              this.selectedHMOClient = undefined;
-              this.hmoPlan.reset();
-            }
-          })
-        }
-      });
+    this.subscribeToValueChanges();
   }
   addInsurancePlan() {
     this.insurancePlanForm = this.formBuilder.group({
@@ -88,15 +62,99 @@ export class PatientPaymentPlanComponent implements OnInit {
           hmo: ['', [<any>Validators.required]],
           hmoPlan: ['', [<any>Validators.required]],
           hmoPlanId: ['', [<any>Validators.required]],
+          plans: [''],
+          client: [''],
           readOnly: [false]
         })
       ])
     });
   }
-  getRole() {
-    if (this.selectedHMOClient !== undefined) {
+  subscribeToValueChanges() {
+    (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[this.insuranceFormArrayIndex]).controls['hmo'].valueChanges.subscribe(value => {
+      this.selectedHMO = value;
+      this.hmoService.hmos(this.selectedFacility._id, value).then(payload => {
+        console.log(payload.body);
+        this.plans = payload.body;
+        (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[this.insuranceFormArrayIndex]).controls['plans'].setValue(this.plans, { onlySelf: true });
+      });
+    });
+    (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[this.insuranceFormArrayIndex]).controls['hmoPlanId'].valueChanges
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .subscribe(value => {
+        console.log(value);
+        this.selectedHMO = (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls[this.insuranceFormArrayIndex]).controls['hmo'].value;
+        if (this.selectedHMO === undefined) {
+          this._notification('Warning', 'Please select an HMO to search from and try again!');
+        } else {
+          this.hmoService.hmos(this.selectedFacility._id, this.selectedHMO, value).then(payload => {
+            console.log(payload);
+            if (payload.body !== null) {
+              this.selectedHMOClient = payload.body;
+              (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray'])
+                .controls[this.insuranceFormArrayIndex]).controls['client'].setValue(payload.body);
+              (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray'])
+                .controls[this.insuranceFormArrayIndex]).controls['hmoPlan'].setValue(payload.body.plan);
+            } else {
+              this.selectedHMOClient = undefined;
+              (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray'])
+                .controls[this.insuranceFormArrayIndex]).controls['hmoPlanId'].setErrors({ idNotFound: true });
+              (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray'])
+                .controls[this.insuranceFormArrayIndex]).controls['hmoPlan'].reset();
+            }
+          })
+        }
+      });
+  }
+  pushNewInsurancePlan() {
+    (<FormArray>this.insurancePlanForm.controls['insurancePlanArray'])
+      .push(
+      this.formBuilder.group({
+        hmo: ['', [<any>Validators.required]],
+        hmoPlan: ['', [<any>Validators.required]],
+        hmoPlanId: ['', [<any>Validators.required]],
+        plans: [''],
+        client: [''],
+        readOnly: [false]
+      })
+      );
+    const index = (<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls.length - 1;
+    this.insuranceFormArrayIndex = index;
+    this.subscribeToValueChanges();
+    (<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).markAsPristine();
+  }
+  onSubmit(insurance, i) {
+    this.selectedHMO = (<FormGroup>(<FormArray>this.insurancePlanForm.controls['insurancePlanArray'])
+      .controls[this.insuranceFormArrayIndex]).controls['readOnly'].setValue(true);
+    console.log(insurance)
+  }
+  isLastChild(i) {
+    const len = (<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls.length;
+    console.log(i);
+    console.log(len)
 
-      let filNo = this.selectedHMOClient.filNo;
+    if (i === (len - 1)){
+      console.log('on')
+      return true;
+    }
+    console.log('off')
+    return false;
+  }
+  closeInsurancePlan(insurance, i) {
+    (<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls.splice(i, 1);
+    if ((<FormArray>this.insurancePlanForm.controls['insurancePlanArray']).controls.length === 0) {
+      this.insuranceFormArrayIndex = 0;
+      this.addInsurancePlan()
+    }
+  }
+  formArrayControlChanges(insurance, i) {
+    this.insuranceFormArrayIndex = i;
+    console.log(i)
+  }
+  getRole(client) {
+    if (client !== undefined) {
+
+      let filNo = client.filNo;
       if (filNo !== undefined) {
         const filNoLength = filNo.length;
         const lastCharacter = filNo[filNoLength - 1];
