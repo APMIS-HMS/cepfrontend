02ca23baac3227c0981e07fdbc4c89bf50d5e584
sabaoth <sunday@apmis.ng>
2017-09-08 15:16:42 +0100

    updates@starday

--- src/app/system-modules/module-menu/health-coverage/company-cover/company-beneficiary-list/company-beneficiary-list.component.ts
+++ src/app/system-modules/module-menu/health-coverage/company-cover/company-beneficiary-list/company-beneficiary-list.component.ts
@@ -3,8 +3,8 @@ import { CoolLocalStorage } from 'angular2-cool-storage';
 import { MdPaginator } from '@angular/material';
 import { FacilityCompanyCoverService } from './../../../../../services/facility-manager/setup/facility-company-cover.service';
 import { Component, OnInit, EventEmitter, Output, Renderer, ElementRef, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-
+import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@angular/forms';
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 @Component({
   selector: 'app-company-beneficiary-list',
   templateUrl: './company-beneficiary-list.component.html',
@@ -20,41 +20,77 @@ export class CompanyBeneficiaryListComponent implements OnInit {
 
   selectedFacility: any = <any>{};
   beneficiaries: any[] = [];
-  filteredBeneficiaries:any[] = [];
-  operateBeneficiaries:any[] = [];
+  filteredBeneficiaries: any[] = [];
+  operateBeneficiaries: any[] = [];
   selectedCompanyCover: any = <any>{};
 
   pageSize = 10;
   pageSizeOptions = [5, 10, 25, 100];
   @ViewChild(MdPaginator) paginator: MdPaginator;
   constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private locker: CoolLocalStorage,
-    private companyCoverService:FacilityCompanyCoverService) { }
+    private companyCoverService: FacilityCompanyCoverService) { }
 
   ngOnInit() {
     this.frmNewBeneficiary = this.formBuilder.group({
       surname: ['', [Validators.required]],
       othernames: ['', [Validators.required]],
       address: ['', [Validators.required]],
-      email: ['', [, <any>Validators.pattern('^([a-z0-9_\.-]+)@([\da-z\.-]+)(com|org|CO.UK|co.uk|net|mil|edu|ng|COM|ORG|NET|MIL|EDU|NG)$')]],
+      email: ['', [<any>Validators.pattern(EMAIL_REGEX)]],
       phone: ['', [<any>Validators.required]],
       principalGender: ['', [<any>Validators.required]],
       principalstatus: ['', [<any>Validators.required]],
       principalEmpID: ['', [<any>Validators.required]]
     });
 
-    this.frmDependant = this.formBuilder.group({
-      dependantName: ['', [Validators.required]],
-      dependantGender: ['', [Validators.required]],
-      dependantEmail: ['', [<any>Validators.required, <any>Validators.pattern('^([a-z0-9_\.-]+)@([\da-z\.-]+)(com|org|CO.UK|co.uk|net|mil|edu|ng|COM|ORG|NET|MIL|EDU|NG)$')]],
-      dependantPhone: ['', [<any>Validators.required]],
-      dependantStatus: ['', [<any>Validators.required]]
-    });
+    // this.frmDependant = this.formBuilder.group({
+    //   dependantSurname: ['', [Validators.required]],
+    //   dependantOthernames: ['', [Validators.required]],
+    //   dependantGender: ['', [Validators.required]],
+    //   dependantEmail: ['', [<any>Validators.pattern(EMAIL_REGEX)]],
+    //   dependantPhone: ['', [<any>Validators.required]],
+    //   dependantStatus: ['', [<any>Validators.required]]
+    // });
+    this.addDependant();
     this.route.params.subscribe(parameters => {
       this.getBeneficiaryList(parameters.id);
     })
   }
-
-  newBeneficiary_show(){
+  addDependant() {
+    this.frmDependant = this.formBuilder.group({
+      'dependantArray': this.formBuilder.array([
+        this.formBuilder.group({
+          dependantSurname: ['', [Validators.required]],
+          dependantOthernames: ['', [Validators.required]],
+          dependantGender: ['', [Validators.required]],
+          dependantEmail: ['', [<any>Validators.pattern(EMAIL_REGEX)]],
+          dependantPhone: ['', [<any>Validators.required]],
+          dependantStatus: ['', [<any>Validators.required]],
+          readOnly: [false]
+        })
+      ])
+    });
+  }
+  pushNewDependant() {
+    (<FormArray>this.frmDependant.controls['dependantArray'])
+      .push(
+      this.formBuilder.group({
+        dependantSurname: ['', [Validators.required]],
+        dependantOthernames: ['', [Validators.required]],
+        dependantGender: ['', [Validators.required]],
+        dependantEmail: ['', [<any>Validators.pattern(EMAIL_REGEX)]],
+        dependantPhone: ['', [<any>Validators.required]],
+        dependantStatus: ['', [<any>Validators.required]],
+        readOnly: [false]
+      })
+      );
+  }
+  closeDependant(dependant, i) {
+    (<FormArray>this.frmDependant.controls['dependantArray']).controls.splice(i, 1);
+    if ((<FormArray>this.frmDependant.controls['dependantArray']).controls.length === 0) {
+      this.addDependant()
+    }
+  }
+  newBeneficiary_show() {
     this.newBeneficiary = !this.newBeneficiary;
   }
 
