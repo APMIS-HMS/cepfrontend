a8c8b8699365ae114bb8e3dae532261e9d0a83cf
starday01 <sunday@apmis.ng>
2017-12-27 17:47:03 +0100

    updates to facility module resource

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 const users = require('./users/users.service.js');
+const facilityModules = require('./facility-modules/facility-modules.service.js');
 const facilityOwnerships = require('./facility-ownerships/facility-ownerships.service.js');
 const facilityTypes = require('./facility-types/facility-types.service.js');
 const facilityClasses = require('./facility-classes/facility-classes.service.js');
@@ -7,4 +8,5 @@ module.exports = function (app) {
   app.configure(facilityOwnerships);
   app.configure(facilityTypes);
   app.configure(facilityClasses);
+  app.configure(facilityModules);
 };
--- test/services/facility-modules.test.js
+++ test/services/facility-modules.test.js
@@ -5,7 +5,6 @@ describe('\'facility-modules\' service', () => {
   it('run create facility module', () => {
     const service = app.service('facility-modules');
     service.create({name: 'Medical Records'}).then(facilityModule =>{
-      
       assert.ok(facilityModule._id);
       assert.strictEqual(facilityModule.name, 'Medical Records');
     });
