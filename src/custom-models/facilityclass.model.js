a9b1b70bc0a800b7b599e8564ff8e5fb520b6f0a
Apmis Mobile <apmismobile.dev@gmail.com>
2017-12-27 17:02:38 +0100

    update@added facility class service

--- src/custom-models/facilityclass.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const facilityClassSchema = new Schema({
-    name: { type: String, required: true },
-    createdAt: { type: Date, 'default': Date.now },
-    updatedAt: { type: Date, 'default': Date.now }
-})
-module.exports = facilityClassSchema;
