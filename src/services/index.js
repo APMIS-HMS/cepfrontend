66051a380fb5d4f537aa97b1e9319df1626e87e4
Apmis Mobile <apmismobile.dev@gmail.com>
2017-12-27 18:49:18 +0100

    update@added gender

--- src/services/index.js
+++ src/services/index.js
@@ -3,10 +3,12 @@ const facilityModules = require('./facility-modules/facility-modules.service.js'
 const facilityOwnerships = require('./facility-ownerships/facility-ownerships.service.js');
 const facilityTypes = require('./facility-types/facility-types.service.js');
 const facilityClasses = require('./facility-classes/facility-classes.service.js');
+const genders = require('./genders/genders.service.js');
 module.exports = function (app) {
   app.configure(users);
   app.configure(facilityOwnerships);
   app.configure(facilityTypes);
   app.configure(facilityClasses);
   app.configure(facilityModules);
+  app.configure(genders);
 };
