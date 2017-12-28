bd8d2926072e4f59b6aff11ee6626563ac6bd6fc
starday01 <sunday@apmis.ng>
2017-12-28 10:26:16 +0100

    updates

--- src/models/locations.model.js
+++ src/models/locations.model.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const locations = new Schema({
-    text: { type: String, required: true }
+    name: { type: String, required: true }
   }, {
     timestamps: true
   });
