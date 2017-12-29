d9f1401ed2e950c44e3c6bcdd4993f7df6a1bb9d
Apmis Mobile <apmismobile.dev@gmail.com>
2017-12-29 08:49:24 +0100

    update@added countries service

---
+++ src/custom-models/state-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const LGSchema = require('./lg-model');
+const citySchema = require('./city-model');
+
+const stateSchema = new Schema({
+  name: { type: String, required: true },
+  lgs: [LGSchema],
+  cities:[citySchema],
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+module.exports = stateSchema;
