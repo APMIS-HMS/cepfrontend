2c4dfe6fa8558779dc4e161eb94ebc8839bf671f
aiyk <aiyk.ekwe@gmail.com>
2017-12-31 14:41:30 +0100

    person account redo

--- src/app/person-account/person-account.component.ts
+++ src/app/person-account/person-account.component.ts
@@ -38,19 +38,17 @@ export class PersonAccountComponent implements OnInit {
     this.getGenders();
     this.getCountries();
     this.frmPerson = this.formBuilder.group({
-
+      persontitle: [new Date(), [<any>Validators.required]],
       firstname: ['', [<any>Validators.required, <any>Validators.minLength(3), <any>Validators.maxLength(50)]],
       lastname: ['', [<any>Validators.required, <any>Validators.minLength(3), <any>Validators.maxLength(50)]],
-      othernames: ['', [<any>Validators.required, <any>Validators.minLength(3), <any>Validators.maxLength(200)]],
-      // password: ['', [<any>Validators.required, <any>Validators.minLength(6), <any>Validators.maxLength(20)]],
-      // repassword: ['', [<any>Validators.required, <any>Validators.minLength(6), <any>Validators.maxLength(20)]],
+      // othernames: ['', [<any>Validators.required, <any>Validators.minLength(3), <any>Validators.maxLength(200)]],
       gender: [[<any>Validators.minLength(2)]],
       dob: [new Date(), [<any>Validators.required]],
-      nationality: ['', [<any>Validators.required]],
+      // nationality: ['', [<any>Validators.required]],
 
-      state: ['', [<any>Validators.required]],
-      lga: ['', [<any>Validators.required]],
-      address: ['', [<any>Validators.required]],
+      // state: ['', [<any>Validators.required]],
+      // lga: ['', [<any>Validators.required]],
+      // address: ['', [<any>Validators.required]],
       email: ['', [<any>Validators.required, <any>Validators.pattern('^([a-z0-9_\.-]+)@([\da-z\.-]+)(com|org|CO.UK|co.uk|net|mil|edu|ng|COM|ORG|NET|MIL|EDU|NG)$')]],
       phone: ['', [<any>Validators.required]]
     });
