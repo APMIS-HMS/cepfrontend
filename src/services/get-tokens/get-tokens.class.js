ae4a3cb99dddd7de9e3d5d8afb0d8c24d3522ce9
Apmis Mobile <apmismobile.dev@gmail.com>
2017-12-31 05:01:03 +0100

    update@undo commited changes from devlopment to features-apmisNo + added  apmisId to token-label property and function to generate apmisNo to get-token service

--- src/services/get-tokens/get-tokens.class.js
+++ src/services/get-tokens/get-tokens.class.js
@@ -15,21 +15,56 @@ class Service {
     return otp;
   }
 
-  get(param) {
+  makeid() {
+    var text = '';
+    var possible = '';
+    for (var i = 0; i < 2; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+  getApmisId() {
+    var number = Math.floor(Math.random() * 99999) + 1;
+    if (number.length <= 5) {
+      number = String('00000' + number).slice(-5);
+    }
+    var retVal = this.makeid() + '-' + number;
+    return retVal;
+  }
+
+  generateId(data) {
+    let apmisNo = this.getApmisId();
+    return this.app.service('people').find({
+      query: {
+        apmisId: apmisNo
+      }
+    }).then(personsApmisReturn => {
+      if (personsApmisReturn.data.length == 0) {
+        data.apmisId = apmisNo;
+      } else {
+        return this.generateId(data);
+      }
+    });
+  }
+
+  get(id, param) {
     let data = {
       token: 0
     };
-    if (param.query.label.toString() === tokenLabel.tokenType.facilityVerification.toString()) {
+    if (id.toString() === tokenLabel.tokenType.facilityVerification.toString()) {
       data.token = this.generateOtp();
+    } else if (param.query.label.toString() === tokenLabel.tokenType.apmisId.toString()) {
+      this.generateId(data);
     }
     return Promise.resolve(data);
   }
 
-  find(param) {
+  find(id, param) {
     let data = {
       token: 0
     };
-    if (param.query.label.toString() === tokenLabel.tokenType.facilityVerification.toString()) {
+    if (id.toString() === tokenLabel.tokenType.facilityVerification.toString()) {
       data.token = this.generateOtp();
     }
     return Promise.resolve(data);
