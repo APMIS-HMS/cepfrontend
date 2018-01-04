82db8b681ee401ba74fa5c3310732e1d7b900621
sabaoth <sunday@apmis.ng>
2018-01-04 13:33:27 +0100

    updates

--- src/app/services/facility-manager/setup/facility-service-category.service.ts
+++ src/app/services/facility-manager/setup/facility-service-category.service.ts
@@ -20,8 +20,8 @@ export class FacilitiesServiceCategoryService {
     private sanitizer: DomSanitizer,
     private locker: CoolLocalStorage
   ) {
-    this._rest = _restService.getService('facilityservices');
-    this._socket = _socketService.getService('facilityservices');
+    this._rest = _restService.getService('organisation-services');
+    this._socket = _socketService.getService('organisation-services');
     this._socket.timeout = 30000;
     this.createListener = Observable.fromEvent(this._socket, 'created');
     this.listner = Observable.fromEvent(this._socket, 'updated');
