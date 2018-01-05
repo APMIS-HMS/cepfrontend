e73e90ba474e0b67576795e4024901184806872e
Simdi <ezeanietochisimdi@gmail.com>
2018-01-05 15:23:24 +0100

    Updated

--- src/app/system-modules/module-menu/forms-manager/treatement-template/template-physician-order/template-physician-order.component.ts
+++ src/app/system-modules/module-menu/forms-manager/treatement-template/template-physician-order/template-physician-order.component.ts
@@ -2,44 +2,29 @@ import { Component, OnInit, EventEmitter, Output, Input, ElementRef } from '@ang
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-template-physician-order',
-  templateUrl: './template-physician-order.component.html',
-  styleUrls: ['./template-physician-order.component.scss']
+  selector: "app-template-physician-order",
+  templateUrl: "./template-physician-order.component.html",
+  styleUrls: ["./template-physician-order.component.scss"]
 })
 export class TemplatePhysicianOrderComponent implements OnInit {
-
   addPhysicianOrderForm: FormGroup;
   addProcedureForm: FormGroup;
   apmisLookupQuery = {};
-  apmisLookupUrl = '';
-  apmisLookupDisplayKey = '';
-  apmisLookupText = '';
+  apmisLookupUrl = "";
+  apmisLookupDisplayKey = "";
+  apmisLookupText = "";
 
   newTemplate = true;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.addPhysicianOrderForm = this.fb.group({
-      physicianOrder: ['', [<any>Validators.required]]
+      physicianOrder: ["", [<any>Validators.required]]
     });
   }
 
-  apmisLookupHandleSelectedItem(value) {
-    this.apmisLookupText = value.name;
-    let isExisting = false;
-    // this.loginHMOListObject.companyCovers.forEach(item => {
-    //   if (item._id === value._id) {
-    //     isExisting = true;
-    //   }
-    // });
-    // if (!isExisting) {
-    //   this.selectedCompanyCover = value;
-    // } else {
-    //   this.selectedCompanyCover = <any>{};
-    //   this._notification('Info', 'Selected HMO is already in your list of Company Covers');
-    // }
+  onClickAddPhysicianOrder(valid: boolean, value: any) {
+    console.log(value);
   }
-
 }
-  
