76b788f0dcc7b38562f00be016937323cee1a681
aiyk <aiyk.ekwe@gmail.com>
2018-01-08 09:20:49 +0100

    modal fixes

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -10,11 +10,11 @@ import { Injectable } from '@angular/core';
 const rx = require('feathers-reactive');
 const RxJS = require('rxjs/Rx');
 
-// const HOST = 'http://13.84.217.251:8082'; // thn
+const HOST = 'http://13.84.217.251:8082'; // thn
 // const HOST = 'http://172.16.16.35:3030'; // Mr Segun
 // const HOST = 'http://40.68.100.29:3030'; // Online
 // const HOST = 'http://192.168.20.101:3030'; // Sunday
-const HOST = 'http://localhost:3030'; // Local Server
+//const HOST = 'http://localhost:3030'; // Local Server
 
 
 
