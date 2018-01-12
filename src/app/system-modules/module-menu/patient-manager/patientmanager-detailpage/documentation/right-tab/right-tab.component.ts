61ba0ab8f8ad2d30f672e8aa4181061d63b880a7
starday01 <sunday@apmis.ng>
2018-01-12 16:03:30 +0100

    updates to documentation

--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/documentation/right-tab/right-tab.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/documentation/right-tab/right-tab.component.ts
@@ -127,7 +127,9 @@ export class RightTabComponent implements OnInit {
         this.patientDocumentation.documentations.forEach(documentation => {
             if (documentation.document.documentType !== undefined && documentation.document.documentType.title === 'Problems') {
                 documentation.document.body.problems.forEach(problem => {
-                    this.problems.push(problem);
+                    if (problem.status.name === 'Active') {
+                        this.problems.push(problem);
+                    }
                 })
             }
         });
