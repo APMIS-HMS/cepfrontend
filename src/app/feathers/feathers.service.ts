be1da5be67a252e35548f2a1dcdd3884e918b6ef
Apmis Mobile <apmismobile.dev@gmail.com>
2018-01-16 09:44:35 +0100

    update

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -12,8 +12,8 @@ const RxJS = require('rxjs/Rx');
 
 // const HOST = 'http://13.84.217.251:8082'; // thn;
 // const HOST = 'http://40.68.100.29:3030'; // Onlineng
-const HOST = 'http://172.16.16.51:3030'; // Sunday
-//const HOST = 'http://localhost:3030'; // Local Server
+// const HOST = 'http://172.16.16.51:3030'; // Sunday
+const HOST = 'http://localhost:3030'; // Local Server
 
 
 
