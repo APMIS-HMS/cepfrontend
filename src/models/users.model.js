ffb9fc20d9254c76431c60a2187e56f5e689e949
Liztank <elizabeth.abbah@gmail.com>
2018-01-18 13:47:34 +0100

    user model modified

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -4,16 +4,36 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const roleSchema = require('../custom-models/role-model');
+  const {
+    Schema
+  } = mongooseClient;
+  //const roleSchema = require('../custom-models/role-model');
 
   const users = new mongooseClient.Schema({
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    personId: { type: Schema.Types.ObjectId, required: true },
-    passwordToken: { type: String, required: false },
-    isTokenVerified: { type: Boolean, 'default': false },
-    facilitiesRole: [roleSchema]
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    personId: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    passwordToken: {
+      type: String,
+      required: false
+    },
+    isTokenVerified: {
+      type: Boolean,
+      'default': false
+    },
+    facilitiesRole: [{
+      type: String
+    }]
 
   }, {
     timestamps: true
