9eb1ee5de125072567ceadafd7a584f87eb8528a
aiyk <aiyk.ekwe@gmail.com>
2018-01-19 13:53:01 +0100

    comment

--- src/app/person-account/person-account.component.ts
+++ src/app/person-account/person-account.component.ts
@@ -160,13 +160,13 @@ export class PersonAccountComponent implements OnInit {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
 
     console.log(this.state);
-    this.counterSubscription = Observable.interval(1000).throttleTime(1000).subscribe(rx => {
-      this.countDown = this.countDown - 1;
-      if (rx === 9) {
-        this.close_onClick();
-        this.counterSubscription.unsubscribe();
-      }
-    });
+    // this.counterSubscription = Observable.interval(1000).throttleTime(1000).subscribe(rx => {
+    //   this.countDown = this.countDown - 1;
+    //   if (rx === 9) {
+    //     this.close_onClick();
+    //     this.counterSubscription.unsubscribe();
+    //   }
+    // });
 
   }
   animateShow() {
