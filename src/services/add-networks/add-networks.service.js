a9a549e24c5c3313c2e4a26ac360de74bb9671f5
Apmis Mobile <apmismobile.dev@gmail.com>
2018-01-19 13:46:25 +0100

    Update@Added a service to add network members

---
+++ src/services/add-networks/add-networks.service.js
@@ -0,0 +1,21 @@
+// Initializes the `addNetworks` service on path `/add-networks`
+const createService = require('./add-networks.class.js');
+const hooks = require('./add-networks.hooks');
+
+module.exports = function (app) {
+  
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'add-networks',
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/add-networks', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('add-networks');
+
+  service.hooks(hooks);
+};
