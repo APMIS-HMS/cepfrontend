2c846c2523a846900a468286b3e71f99887bd84f
Apmis Mobile <apmismobile.dev@gmail.com>
2018-01-22 10:32:46 +0100

    update@Completed networks service

--- src/services/add-networks/add-networks.class.js
+++ src/services/add-networks/add-networks.class.js
@@ -1,57 +1,69 @@
 /* eslint-disable no-unused-vars */
+const logger = require('winston');
 class Service {
-  constructor(options) {
+  constructor (options) {
     this.options = options || {};
-  }
-
-  find(params) {
-    return Promise.resolve([]);
-  }
-
-  get(id, params) {
-    return Promise.resolve({
-      id,
-      text: `A new message with ID: ${id}!`
-    });
-  }
-
-  create(data, params) {
-    const facilitiesService = this.app.service('facilities');
-    var results = [];
-    if (Array.isArray(data)) {
-      return Promise.all(data.memberFacilities.map(current => {
-        facilitiesService.get(current, {}).then(networkMember => {
-          networkMember.memberof.push(data.hostId);
-          facilitiesService.update(networkMember,networkMember._id).then(networkMember => {
-            results.push(networkMember);
-            facilitiesService.get(data.hostId, {}).then(networkHost => {
-              networkHost.memberFacilities.push(current);
-              facilitiesService.update(networkHost,networkHost._id);
-            });
-          });
-        });
-      }));
-    }
-
-    return Promise.resolve(networkMember);
-  }
-
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  remove(id, params) {
-    return Promise.resolve({
-      id
-    });
   }
 
   setup(app) {
     this.app = app;
+  }
+
+  find (params) {
+    return Promise.resolve([]);
+  }
+
+  get (id, params) {
+    return Promise.resolve({
+      id, text: `A new message with ID: ${id}!`
+    });
+  }
+
+  create (data, params) {
+    const facilitiesService = this.app.service('facilities');
+    var results = [];
+    var errors = [];
+    var successes = [];
+    return new Promise(function (resolve, reject) {
+      data.memberFacilities.forEach((current, i) => {
+        facilitiesService.get(current, {}).then(networkMember => {
+          let memberof = [];
+          memberof.push(data.hostId);
+          facilitiesService.patch(networkMember._id,{ memberof: memberof }).then(networkMember => {
+            results.push(networkMember);
+            facilitiesService.get(data.hostId, {}).then(networkHost => {
+              let memberFacilities = [];
+              memberFacilities.push(current);
+              facilitiesService.patch(networkHost._id,{ memberFacilities: memberFacilities }).then(payload => {
+                successes.push(payload);
+                if (i == data.memberFacilities.length - 1) {
+                  resolve(successes);
+                }
+              }, error => {
+                errors.push(error);
+                if (i == data.memberFacilities.length - 1) {
+                  reject(errors);
+                }
+              });
+            });
+          },error=>{
+            //logger.info(error);
+          });
+        });
+      });
+    });
+  }
+
+  update (id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  patch (id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  remove (id, params) {
+    return Promise.resolve({ id });
   }
 }
 
