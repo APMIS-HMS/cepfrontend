899c141a8411fbafd12e58859291c4e2500a30ce
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-01-29 10:13:30 +0100

    s

--- src/app/system-modules/module-menu/facility-page/employees/employeemanager-detailpage/employee-access-roles/employee-access-roles.component.ts
+++ src/app/system-modules/module-menu/facility-page/employees/employeemanager-detailpage/employee-access-roles/employee-access-roles.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
+import {
+  FeatureModuleService
+} from '../../../../../../services/module-manager/setup/index';
+
 
 @Component({
   selector: 'app-employee-access-roles',
@@ -9,21 +13,35 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class EmployeeAccessRolesComponent implements OnInit {
 
-  public userPrivileges: FormGroup;  FormBuilder;
+  public userPrivileges: FormGroup; FormBuilder;
   @Output() closeModal: EventEmitter<boolean> = new EventEmitter<boolean>();
   mainErr = true;
   errMsg = "";
+  roles: any;
 
-  constructor( private formBuilder: FormBuilder,) { }
+  constructor(private formBuilder: FormBuilder, private featureService: FeatureModuleService) { }
 
   ngOnInit() {
-    this.userPrivileges = this.FormBuilder.group({
+    /* this.userPrivileges = this.FormBuilder.group({
 
-  });
-    
+    }); */
+
+    this.getRoles();
+
   }
 
   close_onClick(event) {
     this.closeModal.emit(true);
   }
+
+  getRoles(){
+    this.featureService.findAll().then(payload => {
+      console.log(payload);
+      this.roles = payload.data;
+    });
+  }
+
+
+
+
 }
