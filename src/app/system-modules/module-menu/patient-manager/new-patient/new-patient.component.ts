c85f11240485357e7e481d9fbce3e194005da84b
sabaoth <sunday@apmis.ng>
2018-02-02 10:23:23 +0100

    Remove security question from creating patient

--- src/app/system-modules/module-menu/patient-manager/new-patient/new-patient.component.ts
+++ src/app/system-modules/module-menu/patient-manager/new-patient/new-patient.component.ts
@@ -300,8 +300,8 @@ export class NewPatientComponent implements OnInit, AfterViewInit {
             gender: [[<any>Validators.minLength(2)]],
             dob: [new Date(), [<any>Validators.required]],
             motherMaidenName: ['', [<any>Validators.required, <any>Validators.minLength(3), <any>Validators.maxLength(50), Validators.pattern(ALPHABET_REGEX)]],
-            securityQuestion: ['', [<any>Validators.required]],
-            securityAnswer: ['', [<any>Validators.required]],
+            // securityQuestion: ['', [<any>Validators.required]],
+            // securityAnswer: ['', [<any>Validators.required]],
             // email: ['', [<any>Validators.pattern(EMAIL_REGEX)]],
             phone: ['', [<any>Validators.required, <any>Validators.pattern(PHONE_REGEX)]]
         });
@@ -1830,8 +1830,8 @@ export class NewPatientComponent implements OnInit, AfterViewInit {
                 motherMaidenName: this.titleCasePipe.transform(this.frmPerson.controls['motherMaidenName'].value),
                 // email: this.frmPerson.controls['email'].value,
                 primaryContactPhoneNo: this.frmPerson.controls['phone'].value,
-                securityQuestion: this.frmPerson.controls['securityQuestion'].value,
-                securityAnswer: this.titleCasePipe.transform(this.frmPerson.controls['securityAnswer'].value)
+                // securityQuestion: this.frmPerson.controls['securityQuestion'].value,
+                // securityAnswer: this.titleCasePipe.transform(this.frmPerson.controls['securityAnswer'].value)
             };
             let body = {
                 person: personModel
