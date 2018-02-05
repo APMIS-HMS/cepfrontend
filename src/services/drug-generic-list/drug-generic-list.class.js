44815cf2e428c03bcbb89309862fc0e68be29385
Chisimdi Damian Ezeanieto <ezeanietochisimdi@gmail.com>
2018-02-05 11:28:59 +0100

    Started working on drug-generic-list

--- src/services/drug-generic-list/drug-generic-list.class.js
+++ src/services/drug-generic-list/drug-generic-list.class.js
@@ -1,83 +1,86 @@
 /* eslint-disable no-unused-vars */
 const console = require('console');
-const toLowerCase = require('../../helpers/toLower');
-var Client = require('node-rest-client').Client;
-var jsend = require('jsend');
+const request = require('request');
+const requestPromise = require('request-promise');
+const jsend = require('jsend');
 
 class Service {
-  constructor(options) {
-    this.options = options || {};
-  }
-
-  async find(params) {
-
-    let url = process.env.EMDEX_BASEURL + '/list/?query=' + params.query.searchtext +
-      '&po=' + params.query.po + '&brandonly=' + params.query.brandonly + '&genericonly=' + params.query.genericonly;
-
-    var client = new Client();
-    var args = {
-      headers: {
-        Authorization: process.env.EMDEX_AUTHORISATION_KEY
-      }
-    };
-    client.get(url, args, function (drugs, response) {
-      if (drugs.results !== undefined) {
-        let dataLength = drugs.results.length;
-        let counter = 0;
-        let resultItems = [];
-
-        drugs.results.forEach(function (element) {
-          counter++;
-          if (element.details.toLowerCase().includes(params.query.searchtext.toLowerCase())) {
-            resultItems.push(element);
-          }
-        });
-
-        if (dataLength === counter) {
-          return jsend.success(resultItems);
-        }
-      } else {
-        return jsend.success([]);
-      }
-    });
-    //return Promise.resolve([]);
-  }
-
-  get(id, params) {
-    return Promise.resolve({
-      // id, text: `A new message with ID: ${id}!`
-    });
-  }
-
-  create(data, params) {
-
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current)));
+    constructor(options) {
+        this.options = options || {};
     }
 
-    return Promise.resolve(data);
-  }
+    async find(params) {
+        let url = process.env.EMDEX_BASEURL + '/list/?query=' + params.query.searchtext +
+            '&po=' + params.query.po + '&brandonly=' + params.query.brandonly + '&genericonly=' + params.query.genericonly;
 
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
+        // var args = { headers: { Authorization: process.env.EMDEX_AUTHORISATION_KEY } };
+        const options = {
+            method: 'GET',
+            uri: url,
+            headers: { Authorization: process.env.EMDEX_AUTHORISATION_KEY }
+        };
+        const makeRequest = await requestPromise(options);
+        console.log('---------- makeRequest -----------');
+        console.log(makeRequest);
+        console.log('---------- End makeRequest -----------');
+        // request.get(url, args, function(drugs, response) {
+        //     if (drugs.results !== undefined) {
+        //         let dataLength = drugs.results.length;
+        //         let counter = 0;
+        //         let resultItems = [];
 
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
+        //         drugs.results.forEach(function(element) {
+        //             counter++;
+        //             if (element.details.toLowerCase().includes(params.query.searchtext.toLowerCase())) {
+        //                 resultItems.push(element);
+        //             }
+        //         });
 
-  remove(id, params) {
-    return Promise.resolve({
-      id
-    });
-  }
-  setup(app) {
-    this.app = app;
-  }
+        //         if (dataLength === counter) {
+        //             return jsend.success(resultItems);
+        //         }
+        //     } else {
+        //         return jsend.success([]);
+        //     }
+        // });
+        //return Promise.resolve([]);
+    }
+
+    get(id, params) {
+        return Promise.resolve({
+            // id, text: `A new message with ID: ${id}!`
+        });
+    }
+
+    create(data, params) {
+
+        if (Array.isArray(data)) {
+            return Promise.all(data.map(current => this.create(current)));
+        }
+
+        return Promise.resolve(data);
+    }
+
+    update(id, data, params) {
+        return Promise.resolve(data);
+    }
+
+    patch(id, data, params) {
+        return Promise.resolve(data);
+    }
+
+    remove(id, params) {
+        return Promise.resolve({
+            id
+        });
+    }
+    setup(app) {
+        this.app = app;
+    }
 }
 
-module.exports = function (options) {
-  return new Service(options);
+module.exports = function(options) {
+    return new Service(options);
 };
 
 module.exports.Service = Service;
