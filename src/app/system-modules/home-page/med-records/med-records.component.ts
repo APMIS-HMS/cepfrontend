1b3492acaaa120270f6a809188619b69873fa4dc
aiyk <aiyk.ekwe@gmail.com>
2018-02-08 10:12:16 +0100

    in page menu

--- src/app/system-modules/home-page/med-records/med-records.component.ts
+++ src/app/system-modules/home-page/med-records/med-records.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedRecordsComponent implements OnInit {
 
+  inmenu_home = true;
+  inmenu_med = false;
+  inmenu_prescription = false;
+  inmenu_consultation = false;
+  inmenu_payment = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  inmenu_home_click(){
+    this.inmenu_home = true;
+    this.inmenu_med = false;
+    this.inmenu_prescription = false;
+    this.inmenu_consultation = false;
+    this.inmenu_payment = false;
+  }
+  inmenu_med_click(){
+    this.inmenu_home = false;
+    this.inmenu_med = true;
+    this.inmenu_prescription = false;
+    this.inmenu_consultation = false;
+    this.inmenu_payment = false;
+  }
+  inmenu_prescription_click(){
+    this.inmenu_home = false;
+    this.inmenu_med = false;
+    this.inmenu_prescription = true;
+    this.inmenu_consultation = false;
+    this.inmenu_payment = false;
+  }
+  inmenu_consultation_click(){
+    this.inmenu_home = false;
+    this.inmenu_med = false;
+    this.inmenu_prescription = false;
+    this.inmenu_consultation = true;
+    this.inmenu_payment = false;
+  }
+  inmenu_payment_click(){
+    this.inmenu_home = false;
+    this.inmenu_med = false;
+    this.inmenu_prescription = false;
+    this.inmenu_consultation = false;
+    this.inmenu_payment = true;
+  }
 }
