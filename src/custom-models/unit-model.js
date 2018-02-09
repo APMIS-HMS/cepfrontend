874b5f9bc4da17c5a0176d45691088e3a345ebe9
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-02-09 17:03:02 +0100

    created clinic-model and unit-model.

--- src/custom-models/unit-model.js
+++ src/custom-models/unit-model.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const clinicScheme = require('./clinic-model');
 const unitSchema = new Schema({
-  name: { type: String, required: true },
-  shortName: { type: String, required: false },
-  description: { type: String, required: false },
-  createdAt: { type: Date, 'default': Date.now },
-  updatedAt: { type: Date, 'default': Date.now }
-});
+   name: { type: String, required: false },
+   shortName: { type: String, required: false },
+   description: { type: String, required: false },
+   isClinic: { type: Boolean, required: false },
+   clinics: [clinicScheme],
+   createdAt: { type: Date, 'default': Date.now },
+   updatedAt: { type: Date, 'default': Date.now }
+})
 module.exports = unitSchema;
