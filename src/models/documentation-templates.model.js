69e9d0f1db7af5d7e59386464b17dcb1b41a33c4
starday01 <sunday@apmis.ng>
2018-02-11 23:18:57 +0100

    updates to documentation  templates and getting login user from save employee custom service

--- src/models/documentation-templates.model.js
+++ src/models/documentation-templates.model.js
@@ -1,19 +1,21 @@
 // documentation-templates-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const documentationTemplates = new Schema({
-    facilityId: { type: Schema.Types.ObjectId, required: false },
-    data: { type: Schema.Types.Mixed, required: true },
-    isEditable: { type: Schema.Types.Boolean, 'default': false },
-    name: { type: String, required: true },
-    form: { type: Schema.Types.ObjectId, required: true },
-  }, {
-    timestamps: true
-  });
+module.exports = function(app) {
+    const mongooseClient = app.get('mongooseClient');
+    const { Schema } = mongooseClient;
+    const documentationTemplates = new Schema({
+        facilityId: { type: Schema.Types.ObjectId, required: false },
+        data: { type: Schema.Types.Mixed, required: true },
+        isEditable: { type: Schema.Types.Boolean, default: false },
+        name: { type: String, required: true },
+        visibility: { type: String, required: true },
+        form: { type: Schema.Types.ObjectId, required: true },
+        userId: { type: Schema.Types.ObjectId, required: true }
+    }, {
+        timestamps: true
+    });
 
-  return mongooseClient.model('documentationTemplates', documentationTemplates);
+    return mongooseClient.model('documentationTemplates', documentationTemplates);
 };
