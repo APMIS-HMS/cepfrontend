15487a648582bca8466907f40da701e969b177e3
sabaoth <sunday@apmis.ng>
2018-02-11 23:21:41 +0100

    Updates to treatment template and getting login user

--- src/app/system-modules/service-facade/auth-facade.service.ts
+++ src/app/system-modules/service-facade/auth-facade.service.ts
@@ -1,59 +1,102 @@
-import { FeatureModuleService } from './../../services/module-manager/setup/feature-module.service';
-import { CoolLocalStorage } from 'angular2-cool-storage/src/cool-local-storage';
-import { SocketService } from './../../feathers/feathers.service';
-import { Injectable } from '@angular/core';
+import { FeatureModuleService } from "./../../services/module-manager/setup/feature-module.service";
+import { CoolLocalStorage } from "angular2-cool-storage/src/cool-local-storage";
+import { SocketService } from "./../../feathers/feathers.service";
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthFacadeService {
-
   logingEmployee: any;
   access: any;
-  constructor(private _socketService: SocketService, private locker: CoolLocalStorage, private featureService: FeatureModuleService, ) { }
+  loginUser: any;
+  constructor(
+    private _socketService: SocketService,
+    private locker: CoolLocalStorage,
+    private featureService: FeatureModuleService
+  ) {}
 
   setLogingEmployee(employee) {
-    this.logingEmployee = employee
+    this.logingEmployee = employee;
+  }
+
+  setLoginUser(user) {
+    this.loginUser = user;
   }
 
   getLogingEmployee() {
-    let facId = this.locker.getObject('fac');
+    let facId = this.locker.getObject("fac");
     let self = this;
 
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
       console.log(self.logingEmployee);
       if (self.logingEmployee !== undefined) {
         resolve(self.logingEmployee);
       } else {
         self._socketService.authenticateService();
-        self._socketService.getService('save-employee').get(facId).then(payload => {
-          if(payload.data !== undefined){
-            self.logingEmployee = payload.data[0];
-            self.setLogingEmployee(payload.data[0]);
-            resolve(self.logingEmployee)
-          }else{
-            resolve(undefined)
-          }
-          
-        }, error => {
-        });
+        self._socketService
+          .getService("save-employee")
+          .get(facId)
+          .then(
+            payload => {
+              if (payload !== undefined) {
+                self.logingEmployee = payload.selectedEmployee;
+                self.setLogingEmployee(payload.selectedEmployee);
+                self.setLoginUser(payload.selectedUser);
+                resolve(self.logingEmployee);
+              } else {
+                resolve(undefined);
+              }
+            },
+            error => {}
+          );
       }
-
-
     });
-
   }
-
-  getUserAccessControls() {
-    let facId = this.locker.getObject('fac'); // TO Do: check if fac is in user's facilityRoles
+  getLogingUser() {
+    let facId = this.locker.getObject("fac");
     let self = this;
-    return new Promise(function (resolve, reject) {
+console.log(facId);
+    return new Promise(function(resolve, reject) {
+      console.log(self.loginUser);
+      if (self.loginUser !== undefined) {
+        console.log(1);
+        resolve(self.loginUser);
+      } else {
+        console.log(2)
+        self._socketService.authenticateService();
+        self._socketService
+          .getService("save-employee")
+          .get(facId)
+          .then(
+            payload => {
+              console.log(payload);
+              if (payload !== undefined) {
+                self.setLogingEmployee(payload.selectedEmployee);
+                self.setLoginUser(payload.selectedUser);
+                resolve(self.loginUser);
+              } else {
+                console.log(3)
+                resolve(undefined);
+              }
+            },
+            error => {}
+          );
+      }
+    });
+  }
+  getUserAccessControls() {
+    let facId = this.locker.getObject("fac"); // TO Do: check if fac is in user's facilityRoles
+    let self = this;
+    return new Promise(function(resolve, reject) {
       if (self.access !== undefined && self.access.modules !== undefined) {
         resolve(self.access);
       } else {
-        self.featureService.getUserRoles({ query: { facilityId: facId } }).then(payload => {
-          self.access = payload;
-          resolve(self.access);
-        }, error => {
-        });
+        self.featureService.getUserRoles({ query: { facilityId: facId } }).then(
+          payload => {
+            self.access = payload;
+            resolve(self.access);
+          },
+          error => {}
+        );
       }
     });
   }
