58b4add1a6d252b4f832949ee4cfd15aadeccbda
aiyk <aiyk.ekwe@gmail.com>
2018-02-12 16:49:12 +0100

    patient portal payment

--- src/app/system-modules/home-page/home-page.module.ts
+++ src/app/system-modules/home-page/home-page.module.ts
@@ -14,6 +14,7 @@ import { MedRecordHomeComponent } from './med-records/med-record-home/med-record
 import { MedRecordDocumentationComponent } from './med-records/med-record-documentation/med-record-documentation.component';
 import { MedRecordPrescriptionComponent } from './med-records/med-record-prescription/med-record-prescription.component';
 import { MedRecordDiagnosticsComponent } from './med-records/med-record-diagnostics/med-record-diagnostics.component';
+import { MedRecordPaymentComponent } from './med-records/med-record-payment/med-record-payment.component';
 
 @NgModule({
   imports: [
@@ -33,6 +34,7 @@ import { MedRecordDiagnosticsComponent } from './med-records/med-record-diagnost
     MedRecordDocumentationComponent,
     MedRecordPrescriptionComponent,
     MedRecordDiagnosticsComponent,
+    MedRecordPaymentComponent,
   ]
 })
 export class HomePageModule { }
