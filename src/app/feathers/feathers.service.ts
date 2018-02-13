4116b7daf03a35afdd4957e2c74bb6d94dee7013
sabaoth <sunday@apmis.ng>
2018-02-13 10:55:36 +0100

    updates

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -3,22 +3,14 @@ const feathers = require('@feathersjs/feathers');
 const socketio = require('@feathersjs/socketio-client');
 const io = require('socket.io-client');
 const localstorage = require('feathers-localstorage');
-// const hooks = require('feathers-hooks');
 const rest = require('@feathersjs/rest-client');
 const authentication = require('@feathersjs/authentication-client');
 import { CoolLocalStorage } from 'angular2-cool-storage';
 import { Injectable } from '@angular/core';
 const rx = require('feathers-reactive');
 const RxJS = require('rxjs/Rx');
-// const HOST = 'http://13.84.217.251:8082'; // thn
-<<<<<<< HEAD
-// const HOST = 'http://172.16.16.51:3031'; // Mr Segun
-const HOST = 'https://apmisapitest.azurewebsites.net'; // Online
-=======
-// const HOST = 'http://172.16.16.35:3031'; // Mr Segun
->>>>>>> remotes/origin/development
-// const HOST = 'http://192.168.20.101:3030'; // Sunday
-// const HOST = 'http://localhost:3031'; // Local Server
+
+const HOST = 'https://apmisapitest.azurewebsites.net';
 
 
 @Injectable()
@@ -42,9 +34,7 @@ export class SocketService {
     this.socket = io(this.HOST);
     this._app = feathers()
       .configure(socketio(this.socket))
-      // .configure(rx({ idField: "_id", listStrategy: 'always' }))
       .configure(rx(RxJS, { listStrategy: 'always' }))
-      // .configure(hooks())
       .configure(authentication({ storage: window.localStorage }));
      this._app.on('reauthentication-error', this.errorHandler)
   }
@@ -60,7 +50,6 @@ export class SocketService {
     });
   }
   getService(value: any) {
-    // this._app.authenticate();
     return this._app.service(value);
   }
   authenticateService() {
@@ -85,15 +74,12 @@ export class RestService {
           {
             headers: { 'authorization': 'Bearer ' + auth }
           }
-        )) // Fire up rest
-        // .configure(rx({ idField: '_id', listStrategy: 'always' }))
+        )) 
         .configure(rx(RxJS, { listStrategy: 'always' }))
-        // .configure(hooks())
         .configure(authentication({ storage: window.localStorage }));
     } else {
       this._app = feathers() // Initialize feathers
         .configure(rest(this.HOST).superagent(superagent)) // Fire up rest
-        // .configure(hooks())
         .configure(authentication({ storage: window.localStorage })); // Configure feathers-hooks
     }
   }
@@ -105,7 +91,6 @@ export class RestService {
     });
   }
   getService(value: any) {
-    // this._app.authenticate();
     return this._app.service(value);
   }
   authenticateService() {
