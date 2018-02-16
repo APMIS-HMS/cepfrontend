04fdc83effa91bf2da2c071efb73dc1510f3f64c
lizzy Abbah <lizzyabbah@gmail.com>
2018-02-16 11:51:02 +0100

    update

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -11,9 +11,9 @@ import { Injectable } from '@angular/core';
 const rx = require('feathers-reactive');
 const RxJS = require('rxjs/Rx');
 // const HOST = 'http://13.84.217.251:8082'; // thn
-// const HOST = 'http://172.16.16.35:3031'; // Mr Segun
+ const HOST = 'http://172.16.16.51:3031'; // Mr Segun
 // const HOST = 'http://192.168.20.101:3030'; // Sunday
-const HOST = 'http://localhost:3031'; // Local Server
+//const HOST = 'http://localhost:3031'; // Local Server
 
 
 @Injectable()
