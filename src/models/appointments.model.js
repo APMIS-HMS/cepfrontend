739ca901d4863467989258445c1224220f6bd507
starday01 <sunday@apmis.ng>
2018-02-23 10:43:14 +0100

    Updates to appointment model

--- src/models/appointments.model.js
+++ src/models/appointments.model.js
@@ -1,5 +1,5 @@
 // appointments-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 const attendanceModel = require('../custom-models/attendance');
@@ -7,29 +7,29 @@ const encounterModel = require('../custom-models/encounter');
 const clinicInteractionModel = require('../custom-models/clinic-interaction');
 
 
-module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const appointments = new Schema({
-    facilityId: { type: Schema.Types.ObjectId, required: true },
-    clinicId: { type: Schema.Types.String, required: true },
-    appointmentTypeId: { type: Schema.Types.String, required: true },
-    doctorId: { type: Schema.Types.ObjectId, required: false },
-    orderStatusId: { type: Schema.Types.String, required: true },
-    zoom: { type: Schema.Types.Mixed, required: false },
-    startDate: { type: Date, require: true },
-    patientId: { type: Schema.Types.ObjectId, required: true },
-    appointmentReason: { type: String, require: false },
-    category: { type: Schema.Types.String, required: true },
-    checkedOut: { type: Schema.Types.Mixed, required: false },
-    attendance: attendanceModel,
-    encounters: [encounterModel],
-    clinicInteractions: [clinicInteractionModel],
-    isActive: { type: Boolean, 'default': true },
-    isCheckedOut: { type: Boolean, 'default': false },
-  }, {
-    timestamps: true
-  });
+module.exports = function(app) {
+    const mongooseClient = app.get('mongooseClient');
+    const { Schema } = mongooseClient;
+    const appointments = new Schema({
+        facilityId: { type: Schema.Types.ObjectId, required: true },
+        clinicId: { type: Schema.Types.String, required: true },
+        appointmentTypeId: { type: Schema.Types.String, required: true },
+        doctorId: { type: Schema.Types.ObjectId, required: false },
+        orderStatusId: { type: Schema.Types.String, required: true },
+        zoom: { type: Schema.Types.Mixed, required: false },
+        startDate: { type: Date, require: true },
+        patientId: { type: Schema.Types.ObjectId, required: true },
+        appointmentReason: { type: String, require: false },
+        category: { type: Schema.Types.String, required: true },
+        checkedOut: { type: Schema.Types.Mixed, required: false },
+        attendance: attendanceModel,
+        encounters: [encounterModel],
+        clinicInteractions: [clinicInteractionModel],
+        isActive: { type: Boolean, 'default': true },
+        isCheckedOut: { type: Boolean, 'default': false },
+    }, {
+        timestamps: true
+    });
 
-  return mongooseClient.model('appointments', appointments);
+    return mongooseClient.model('appointments', appointments);
 };
