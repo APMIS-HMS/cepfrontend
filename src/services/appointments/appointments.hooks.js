e8deee34cff64dca50b0db5db2ce9da968ea2684
starday01 <sunday@apmis.ng>
2018-02-23 13:10:24 +0100

    Checked if doctor is picked before running appointment hook

--- src/services/appointments/appointments.hooks.js
+++ src/services/appointments/appointments.hooks.js
@@ -1,51 +1,52 @@
-
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { fastJoin } = require('feathers-hooks-common');
 
 const resolvers = {
-  joins: {
-    patientDetails: () => async (appointment, context) => {
-      const patient = await context.app
-        .service('patients')
-        .get(appointment.patientId, {});
-      appointment.patientDetails = patient;
-    },
-    providerDetails: () => async (appointment, context) => {
-      const employee = await context.app
-        .service('employees')
-        .get(appointment.doctorId, {});
-      appointment.providerDetails = employee;
+    joins: {
+        patientDetails: () => async(appointment, context) => {
+            const patient = await context.app
+                .service('patients')
+                .get(appointment.patientId, {});
+            appointment.patientDetails = patient;
+        },
+        providerDetails: () => async(appointment, context) => {
+            if (appointment.doctorId !== undefined) {
+                const employee = await context.app
+                    .service('employees')
+                    .get(appointment.doctorId, {});
+                appointment.providerDetails = employee;
+            }
+        }
     }
-  }
 };
 module.exports = {
-  before: {
-    all: [authenticate('jwt')],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [authenticate('jwt')],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  after: {
-    all: [fastJoin(resolvers)],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [fastJoin(resolvers)],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    }
 };
