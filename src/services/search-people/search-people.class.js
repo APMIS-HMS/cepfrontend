153a4105ecc73c38fa4d24d70000591f5d8e5213
starday01 <sunday@apmis.ng>
2018-02-27 11:35:14 +0100

    Open Appointment for socket updates, increase facility access control limit, add isEngaged to Appointment Model

--- src/services/search-people/search-people.class.js
+++ src/services/search-people/search-people.class.js
@@ -1,78 +1,56 @@
 /* eslint-disable no-unused-vars */
 const logger = require('winston');
 const jsend = require('jsend');
+var startOfDay = require('date-fns/start_of_day');
+var endOfDay = require('date-fns/end_of_day');
 class Service {
-  constructor(options) {
-    this.options = options || {};
-  }
-
-  async find(params) {
-    const peopleService = this.app.service('people');
-    if (params.query.isValidating === undefined || params.query.isValidating === false) {
-      let returnData = await peopleService.find({
-        query: {
-          apmisId: params.query.apmisId,
-          securityQuestion: params.query.securityQuestion,
-          securityAnswer: params.query.securityAnswer
-        }
-      });
-      if (returnData.data.length > 0) {
-        return returnData.data[0]._id;
-      } else {
-        return undefined;
-      }
-    }else{
-      let returnData = await peopleService.find({
-        query: {
-          firstName: params.query.firstName,
-          lastName: params.query.lastName,
-          motherMaidenName: params.query.motherMaidenName,
-          primaryContactPhoneNo:params.query.primaryContactPhoneNo
-        }
-      });
-      if (returnData.data.length > 0) {
-        return jsend.success(true);
-      } else {
-        return jsend.fail(false);
-      }  
+    constructor(options) {
+        this.options = options || {};
     }
 
+    async find(params) {
+        const peopleService = this.app.service('people');
+        if (params.query.isValidating === undefined || params.query.isValidating === false) {
+            let returnData = await peopleService.find({
+                query: {
+                    apmisId: params.query.apmisId,
+                    securityQuestion: params.query.securityQuestion,
+                    securityAnswer: params.query.securityAnswer
+                }
+            });
+            if (returnData.data.length > 0) {
+                return returnData.data[0]._id;
+            } else {
+                return undefined;
+            }
+        } else {
+            const start = startOfDay(params.query.dateOfBirth);
+            const end = endOfDay(params.query.dateOfBirth);
+            let returnData = await peopleService.find({
+                query: {
+                    firstName: params.query.firstName,
+                    dateOfBirth: { '$gte': start, '$lt': end }, //params.query.dateOfBirth,
+                    motherMaidenName: params.query.motherMaidenName,
+                    gender: params.query.gender
+                }
+            });
+            if (returnData.data.length > 0) {
+                return jsend.success(true);
+            } else {
+                return jsend.fail(false);
+            }
+        }
 
-  }
 
-  get(id, params) {
-    return Promise.resolve({
-      id, text: `A new message with ID: ${id}!`
-    });
-  }
-
-  create(data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current)));
     }
 
-    return Promise.resolve(data);
-  }
-
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  remove(id, params) {
-    return Promise.resolve({ id });
-  }
-
-  setup(app) {
-    this.app = app;
-  }
+    setup(app) {
+        this.app = app;
+    }
 }
 
-module.exports = function (options) {
-  return new Service(options);
+module.exports = function(options) {
+    return new Service(options);
 };
 
 module.exports.Service = Service;
