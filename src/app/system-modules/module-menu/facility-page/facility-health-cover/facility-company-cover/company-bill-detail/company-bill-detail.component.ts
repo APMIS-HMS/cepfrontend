858cab4b7c07ef691e82917d875ffd8228672fa7
sabaoth <sunday@apmis.ng>
2018-03-01 16:57:07 +0100

    Fixed non declare properties

--- src/app/system-modules/module-menu/facility-page/facility-health-cover/facility-company-cover/company-bill-detail/company-bill-detail.component.ts
+++ src/app/system-modules/module-menu/facility-page/facility-health-cover/facility-company-cover/company-bill-detail/company-bill-detail.component.ts
@@ -1,28 +1,25 @@
 import { Component, OnInit, EventEmitter, Output, Input } from "@angular/core";
-import { FormControl } from '@angular/forms';
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-company-bill-detail',
-  templateUrl: './company-bill-detail.component.html',
-  styleUrls: ['./company-bill-detail.component.scss']
+  selector: "app-company-bill-detail",
+  templateUrl: "./company-bill-detail.component.html",
+  styleUrls: ["./company-bill-detail.component.scss"]
 })
 export class CompanyBillDetailComponent implements OnInit {
-
   @Output() closeModal: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   hmoTypeControl: FormControl = new FormControl();
   authCodeControl: FormControl = new FormControl();
+  workspace: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   close_onClick() {
     this.closeModal.emit(true);
   }
 
-  newWorkspace_onClick(employee) { }
+  newWorkspace_onClick(employee) {}
   deletion_popup(workspace) {}
-
 }
