d02610d11efc84a94f13982ff66b071edd88a1bf
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-03-02 16:54:57 +0100

    created a fastJoin on family to get patientObject.

--- src/services/family/family.hooks.js
+++ src/services/family/family.hooks.js
@@ -1,8 +1,32 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { fastJoin } = require('feathers-hooks-common');
+
+
+const resolvers = {
+  joins: {
+    patientObject: () => async (item, context) => {
+      try {
+        console.log(item);
+        if (item.familyCovers !== undefined) {
+          const len = item.familyCovers.length;
+          for (let i = 0; i < len; i++) {
+            if(item.familyCovers[i].patientId !== undefined){
+              console.log('Try If');
+              const patient = await context.app.service('patients').get(item.familyCovers[i].patientId, {});
+              item.familyCovers[i].patientObject = patient;
+            }
+          }
+        }
+      } catch (error) {
+        item.patientObject = {};
+      }
+    }
+  }
+};
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate('jwt')],
     find: [],
     get: [],
     create: [],
@@ -12,7 +36,7 @@ module.exports = {
   },
 
   after: {
-    all: [],
+    all: [fastJoin(resolvers)],
     find: [],
     get: [],
     create: [],
