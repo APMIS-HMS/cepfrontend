6a7668472f8ce03d5fc736fdae0c97bd44f0e28d
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-03-08 15:51:28 +0100

    just starting the uploading .

--- src/services/upload-images/upload-images.class.js
+++ src/services/upload-images/upload-images.class.js
@@ -29,16 +29,17 @@ class Service {
   async create(data, params) {
     const ACCESS_KEY = process.env.AZURE_STORAGE_ACCESS_KEY;
     var blobSvc = azure.createBlobService('apmisstorageaccount', ACCESS_KEY);
-    const fileName = params.fileName;
+    const fileName = data.fileName;
 
     var rawdata = data.base64;
     var matches = rawdata.match(/^data:([A-Za-z-+\\/]+);base64,(.+)$/);
     var type = matches[1];
     var buffer = new Buffer(matches[2], 'base64');
+    const container = data.container;
 
     return new Promise(function (resolve, reject) {
 
-      blobSvc.createBlockBlobFromText('personcontainer', fileName, buffer, { contentType: type }, function (error, result, response) {
+      blobSvc.createBlockBlobFromText(container, fileName, buffer, { contentType: type }, function (error, result, response) {
 
         if (error) {
           reject(error);
