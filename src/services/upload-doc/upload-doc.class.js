5a07b0fb9b29f0dbfa8b52cc108711e56475762f
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-03-12 13:03:50 +0100

    upload doc done.

---
+++ src/services/upload-doc/upload-doc.class.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-unused-vars */
+const logger = require('winston');
+var azure = require('azure-storage');
+var path = require('path');
+
+
+class Service {
+  constructor (options) {
+    this.options = options || {};
+  }
+
+  find (params) {
+    return Promise.resolve([]);
+  }
+
+  get (id, params) {
+    return Promise.resolve({
+      id, text: `A new message with ID: ${id}!`
+    });
+  }
+
+  async create(data, params) {
+    const ACCESS_KEY = process.env.AZURE_STORAGE_ACCESS_KEY;
+    var blobSvc = azure.createBlobService('apmisstorageaccount', ACCESS_KEY);
+    const fileExtArr = data.name.split(".");
+    const fileExt = fileExtArr[fileExtArr.length-1];
+    const fileName = data.labRequestId+'_'+data.investigationId+'_'+data.reportId+'.'+fileExt;
+    var rawdata = data.base64;
+    var matches = rawdata.match(/^data:([A-Za-z-+\\/]+);base64,(.+)$/);
+    var type = matches[1];
+    var buffer = new Buffer(matches[2], 'base64');
+    const container = data.container;
+
+    return new Promise(function (resolve, reject) {
+
+      blobSvc.createBlockBlobFromText(container, fileName, buffer, { contentType: type }, function (error, result, response) {
+        const fileUrl = blobSvc.getUrl(result.container, result.name);
+        if (error) {
+          reject(error);
+        } else {
+          resolve({
+            fileName: fileName,
+            fileUrl: fileUrl,
+            response: response
+          });
+        }
+      });
+    });
+  }
+
+  update (id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  patch (id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  remove (id, params) {
+    return Promise.resolve({ id });
+  }
+}
+
+module.exports = function (options) {
+  return new Service(options);
+};
+
+module.exports.Service = Service;
