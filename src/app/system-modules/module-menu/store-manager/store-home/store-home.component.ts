e149a6ce62869de6cc6aab704ad87d76080d470c
aiyk <aiyk.ekwe@gmail.com>
2018-03-20 15:59:33 +0100

    tool tip

--- src/app/system-modules/module-menu/store-manager/store-home/store-home.component.ts
+++ src/app/system-modules/module-menu/store-manager/store-home/store-home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class StoreHomeComponent implements OnInit {
 
-  Ql_toggle = true;
+  Ql_toggle = false;
 
   constructor() { }
 
