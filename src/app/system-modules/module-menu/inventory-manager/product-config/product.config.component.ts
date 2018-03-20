da3eb732e40540bb5639e71bbbbaf9370e0aa891
Apmis Mobile <apmismobile.dev@gmail.com>
2018-03-20 17:23:08 +0100

    update@complete saving configuration

--- src/app/system-modules/module-menu/inventory-manager/product-config/product.config.component.ts
+++ src/app/system-modules/module-menu/inventory-manager/product-config/product.config.component.ts
@@ -14,12 +14,20 @@ export class ProductConfigComponent implements OnInit {
 
   packages = [];
   products = [];
-  selectedFacility = {};
-  selectedProduct = {};
-  myForm: FormGroup;
+  selectedFacility: any = {};
+  selectedProduct: any = {};
   packageForm: FormGroup;
   searchProdductControl: FormControl = new FormControl();
+  btnSave = new FormControl();
+  btnShowStatus = true;
 
+  apmisLookupUrl = 'products';
+  apmisLookupText = '';
+  apmisLookupQuery = {};
+  apmisLookupDisplayKey = 'name';
+  apmisInvestigationLookupQuery: any = {
+  };
+  control;
   constructor(private _fb: FormBuilder,
     private productService: ProductService,
     private locker: CoolLocalStorage,
@@ -27,30 +35,29 @@ export class ProductConfigComponent implements OnInit {
 
   ngOnInit() {
     this.selectedFacility = <any>this.locker.getObject('selectedFacility');
-    this.myForm = this._fb.group({
-      package: this._fb.array([
+    this.packageForm = this._fb.group({
+      'package': this._fb.array([
+        this._fb.group({
+          name: ['', Validators.required],
+          size: [0, Validators.required],
+          id: ['']
+        })
       ])
     });
+    this.packageForm.controls['package'] = this._fb.array([]);
+
+    const control = <FormArray>this.packageForm.controls['package'];
     this.searchProdductControl.valueChanges
       .debounceTime(400)
       .distinctUntilChanged()
       .subscribe(value => {
-        if (value !== null && value.length === 0) {
-          this.systemModuleService.on();
-          this.productService
-            .find({
-              query: {
-                name: { $regex: this.searchProdductControl.value, $options: 'i' }
-              }
-            })
-            .then(payload => {
-              this.systemModuleService.off();
-              this.products = payload.data;
-            })
-            .catch(err => { });
+        if (value.length >= 3) {
+          this.apmisInvestigationLookupQuery = {
+            name: { $regex: this.searchProdductControl.value, '$options': 'i' },
+          }
         }
       });
-
+    this.getPackagesizes();
   }
 
   getPackagesizes() {
@@ -59,19 +66,76 @@ export class ProductConfigComponent implements OnInit {
     });
   }
 
-  onSelectProduct(value){
-  this.selectedProduct = value;
+  apmisLookupHandleSelectedItem(value) {
+    this.apmisLookupText = value.name;
+    this.selectedProduct = value;
   }
 
-  addPackage(i: number): void {
-    const control = <FormArray>this.myForm.controls['package'];
-    control.push(this.createPackage());
+  onSelectProduct(value) {
+    this.selectedProduct = value;
   }
 
-  createPackage(): FormGroup {
+  addPackage(event, item, i): void {
+    console.log(item);
+    console.log(event.target.checked);
+    if (event.target.checked === true) {
+      (<FormArray>this.packageForm.controls['package'])
+        .push(
+          this._fb.group({
+            name: [item.name, [<any>Validators.required]],
+            size: [0, [<any>Validators.required]],
+            id: [item._id]
+          })
+        );
+    } else {
+      let indexToRemove = 0;
+      (<FormArray>this.packageForm.controls['package']).controls.forEach((item: any, i) => {
+        const packagelValue: any = (<any>item).controls['id'].value;
+        if (packagelValue === item._id || packagelValue === item.id) {
+          indexToRemove = i;
+        }
+      });
+      const count = (<FormArray>this.packageForm.controls['package']).controls.length;
+      if (count === 1) {
+        this.packageForm.controls['package'] = this._fb.array([]);
+      } else {
+        (<FormArray>this.packageForm.controls['package']).controls.splice(indexToRemove, 1);
+      }
+    }
+  }
+
+  createPackage(item): FormGroup {
     return this._fb.group({
-      pack: ['', Validators.required],
+      name: [item.name, Validators.required],
       size: [0, Validators.required]
     });
   }
+
+  save() {
+    if ((<FormArray>this.packageForm.controls['package']).valid) {
+      this.btnShowStatus = false;
+      this.systemModuleService.on();
+      let productConfig: any = {};
+      productConfig.productId = this.selectedProduct._id;
+      productConfig.facilityId = this.selectedFacility._id;
+      productConfig.packSizes = (<FormArray>this.packageForm.controls['package']).value;
+      // (<FormArray>this.packageForm.controls['package']).controls.forEach((itemi, i) => {
+      //   console.log(itemi.value);
+      //   productConfig.packSizes.push(itemi.value);
+      // });
+      this.productService.createProductConfig(productConfig).then(payload => {
+        this.systemModuleService.off();
+        this.btnShowStatus = true;
+        this.systemModuleService.announceSweetProxy('Configuration created', 'success');
+      },err=>{
+        console.log(err);
+        this.systemModuleService.off();
+        this.systemModuleService.announceSweetProxy('Failed to create configuration for product', 'error');
+      });
+    } else {
+      this.systemModuleService.off();
+      this.systemModuleService.announceSweetProxy('One or more field(s) missing', 'error');
+    }
+  }
+
 }
