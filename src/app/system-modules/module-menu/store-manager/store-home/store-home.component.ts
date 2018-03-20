597222bfd81914303f6e1edb9729d9d418e5e4d5
aiyk <aiyk.ekwe@gmail.com>
2018-03-20 15:47:09 +0100

    store landing page

---
+++ src/app/system-modules/module-menu/store-manager/store-home/store-home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-store-home',
+  templateUrl: './store-home.component.html',
+  styleUrls: ['./store-home.component.scss']
+})
+export class StoreHomeComponent implements OnInit {
+
+  Ql_toggle = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleQl(){
+    this.Ql_toggle = !this.Ql_toggle;
+  }
+
+}
