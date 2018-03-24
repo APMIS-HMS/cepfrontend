c51f82f8f791b701c3df1b299565646e49448177
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-03-24 09:01:57 +0100

    assinging a tag to patient has been done with the create in the custom service.

--- src/services/suggest-patient-tags/suggest-patient-tags.class.js
+++ src/services/suggest-patient-tags/suggest-patient-tags.class.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 class Service {
-  constructor (options) {
+  constructor(options) {
     this.options = options || {};
   }
 
-  async find (params) {
+  async find(params) {
     const tagName = params.query.word;
     const facilityId = params.query.facilityId;
     const tagService = this.app.service('service-tags');
@@ -18,12 +18,12 @@ class Service {
     let tagsLength = tagsData.length;
     let tagsArr = [];
 
-    while(tagsLength--){
-      if(tagsData[tagsLength].tagType !== undefined){
-        if(tagsData[tagsLength].tagType.toLowerCase() !== "identification"){
+    while (tagsLength--) {
+      if (tagsData[tagsLength].tagType !== undefined) {
+        if (tagsData[tagsLength].tagType.toLowerCase() !== "identification") {
           tagsArr.push(tagsData[tagsLength]);
         }
-      }else{
+      } else {
         tagsArr.push(tagsData[tagsLength]);
       }
     }
@@ -34,33 +34,67 @@ class Service {
     //return Promise.resolve([]);
   }
 
-  get (id, params) {
+  get(id, params) {
     return Promise.resolve({
       id, text: `A new message with ID: ${id}!`
     });
   }
 
-  create (data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current)));
+  async create(data, params) {
+    const tagService = this.app.service('service-tags');
+    const patientService = this.app.service('patients');
+    const patientId = data.patientId;
+    const identity = data.identity;
+
+    let tag;
+    const checkPatientTags = await patientService.find({
+      query: {
+        _id: patientId
+      }
+    });
+    const patientData = checkPatientTags.data[0];
+
+    tag = await tagService.find({
+      query: {
+        name: data.name,
+        facilityId: data.facilityId
+      }
+    });
+    console.log(tag.data);
+    if (tag.data.length > 0) {
+      if(tag.data[0].tagType === 'identification'){
+        return [];
+      }
+      const patientTags = patientData.tags.filter(x => x.name === data.name);
+      if (patientTags.length > 0) {
+        return [];
+      } else {
+        delete data.patientId;
+        patientData.tags.push(data);
+        const patchedPatient = await patientService.patch(patientData._id, patientData);
+        return patchedPatient;
+      }
+    } else {
+      const createdTag = await tagService.create(data);
+      patientData.tags.push(createdTag);
+      const patchedPatient = await patientService.patch(patientData._id, patientData);
+      return patchedPatient;
     }
+  }
 
+  update(id, data, params) {
     return Promise.resolve(data);
   }
 
-  update (id, data, params) {
+  patch(id, data, params) {
     return Promise.resolve(data);
   }
 
-  patch (id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  remove (id, params) {
+  remove(id, params) {
     return Promise.resolve({ id });
   }
 
-  setup(app){
+  setup(app) {
     this.app = app;
   }
 }
