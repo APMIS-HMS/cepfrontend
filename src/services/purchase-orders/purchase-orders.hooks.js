8b64e07c49b90684c5d422d46b2666219bccc3ed
Apmis Mobile <apmismobile.dev@gmail.com>
2018-03-28 12:16:40 +0100

    update@

--- src/services/purchase-orders/purchase-orders.hooks.js
+++ src/services/purchase-orders/purchase-orders.hooks.js
@@ -9,16 +9,11 @@ const {
 const resolvers = {
   joins: {
     facilityDetails: () => async (data, context) => {
-      const facility = await context.app.service('facilities').find({
-        query: {
-          _id: data.supplierId
-        }
-      });
-      data.supplierObject = facility;
+      const supplier = await context.app.service('suppliers').get(data.supplierId);
+      data.supplierObject = supplier;
     },
     productObject: () => async (data, context) => {
       if (data.orderedProducts !== null || data.orderedProducts !== undefined) {
-        console.log(data.orderedProducts);
         if (data.orderedProducts.length > 0) {
           let len2 = data.orderedProducts.length - 1;
           for (let j = 0; j <= len2; j++) {
@@ -27,7 +22,7 @@ const resolvers = {
                 let getProduct = await context.app.service('products').get(data.orderedProducts[j].productId);
                 data.orderedProducts[j].productObject = getProduct;
               } catch (e) {
-                console.log(e);
+                // console.log(e);
               }
             }
           }
