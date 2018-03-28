9abba301bb0a3643a8dee4e18c452dde7c4d4cdb
Apmis Mobile <apmismobile.dev@gmail.com>
2018-03-28 12:14:39 +0100

    update@fixed purchase order list

--- src/app/system-modules/module-menu/purchase-manager/purchase-order/purchase-order.component.ts
+++ src/app/system-modules/module-menu/purchase-manager/purchase-order/purchase-order.component.ts
@@ -47,6 +47,7 @@ export class PurchaseOrderComponent implements OnInit {
       if (value !== null) {
         this.systemModuleService.on();
         this.purchaseOrderService.find({ query: { supplierId: value, isActive: true } }).subscribe(payload => {
+          console.log(payload);
           if (payload.data != undefined) {
             this.orders = payload.data;
             this.systemModuleService.off();
