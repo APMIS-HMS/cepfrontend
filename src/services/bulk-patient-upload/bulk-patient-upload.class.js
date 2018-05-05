9aff92e242bb647960e86532f1cba93638a48971
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2018-05-05 22:16:11 +0100

    Added hospital Id to the tags of the bulk uploaded patients.

--- src/services/bulk-patient-upload/bulk-patient-upload.class.js
+++ src/services/bulk-patient-upload/bulk-patient-upload.class.js
@@ -21,8 +21,11 @@ class Service {
   async create(data, params) {
     const patientService = this.app.service('patients');
     const savePersonService = this.app.service('save-person');
+    const searchPeopleService = this.app.service('search-people');
+    const patientTagService = this.app.service('suggest-patient-tags');
     let savedPerson;
     let savedPatient;
+    let savedPatientTags;
     let returnData = [];
     let failedAttempts = [];
     let length = data.length;
@@ -31,31 +34,75 @@ class Service {
         let datas = {
           person: data[i]
         }
-        try {
-          savedPerson = await savePersonService.create(datas);
-        } catch (e) {
-          failedAttempts.push(data[i]);
+        let checkPerson = await searchPeopleService.find({
+          query: {
+            firstName: data[i].firstName,
+            motherMaidenName: '',
+            dateOfBirth: data[i].dateOfBirth,
+            gender: data[i].gender,
+            isValidating: true
+          }
+        });
+        console.log(checkPerson, data[i]);
+        if (checkPerson.data === false) {
+          try {
+            savedPerson = await savePersonService.create(datas);
+          } catch (e) {
+            failedAttempts.push({
+              data: data[i],
+              message: 'Error, creating person information'
+            });
+          }
+          let patient = {
+            personId: savedPerson._id,
+            facilityId: data[i].facilityId
+          }
+          if (data[i].payPlan.toLowerCase() === 'wallet') {
+            patient.paymentPlan = [
+              {
+                planType: 'wallet',
+                bearerPersonId: savedPerson._id,
+                isDefault: true
+              }
+            ]
+          }
+          try {
+            savedPatient = await patientService.create(patient);
+          } catch (e) {
+            failedAttempts.push({
+              data: data[i],
+              message: 'Error, creating Patient'
+            });
+          }
+          let dataForPatientTags = {
+            name: data[i].hospId,
+            facilityId: data[i].facilityId,
+            patientId: savedPatient._id,
+            tagType: 'identification'
+          }
+          delete data[i].hospId;;
+          try { 
+            savedPatientTags = await patientTagService.create(dataForPatientTags);
+            returnData.push(savedPatient);
+          } catch (e) { 
+            failedAttempts.push({
+              data: data[i],
+              message: 'Error, Assigning Hospital Id to Patient'
+            });
+          }
+        } else {
+          failedAttempts.push({
+            data: data[i],
+            message: 'Record already exist!'
+          });
         }
-        let patient = {
-          personId: savedPerson._id,
-          facilityId: data[i].facilityId
-        }
-        if (data[i].payPlan.toLowerCase() === 'wallet') {
-          patient.paymentPlan = [
-            {
-              planType: 'wallet',
-              bearerPersonId: savedPerson._id,
-              isDefault: true
-            }
-          ]
-        }
-        try {
-          savedPatient = await patientService.create(patient);
-          returnData.push(savedPatient);
-        } catch (e) {
-          failedAttempts.push(data[i]);
-        }
+
       }
+    }
+
+    return {
+      success: returnData,
+      failed: failedAttempts
     }
   }
 
