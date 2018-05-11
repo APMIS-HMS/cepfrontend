8f4bc97002ee04bba5dc1624edf0eef87844cc82
sabaoth <sunday@apmis.ng>
2018-05-11 08:37:01 +0100

    resolve undeclare property productNavMenu

--- src/app/system-modules/module-menu/payment/payment-home/payment-home.component.ts
+++ src/app/system-modules/module-menu/payment/payment-home/payment-home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PaymentHomeComponent implements OnInit {
 
   pageInView = 'Payment';
-
+  productNavMenu = false;
   constructor() { }
 
   ngOnInit() {
