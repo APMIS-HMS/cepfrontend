5e43763661d3887f399eb668ef8b591ab16498a1
Chisimdi Damian Ezeanieto <ezeanietochisimdi@gmail.com>
2018-05-15 04:04:37 +0100

    Almost done with Immunization Schedule.

--- src/app/system-modules/module-menu/immunization/immunization-schedule/immunization-schedule.component.ts
+++ src/app/system-modules/module-menu/immunization/immunization-schedule/immunization-schedule.component.ts
@@ -24,6 +24,8 @@ export class ImmunizationScheduleComponent implements OnInit {
   disableBtn: boolean = false;
   saveScheduleBtn: boolean = true;
   updateScheduleBtn: boolean = false;
+  savingScheduleBtn: boolean = false;
+  updatingScheduleBtn:boolean = false;
   showCuDropdown: boolean = false;
   cuDropdownLoading: boolean = true;
   drugIndex: number;
@@ -44,21 +46,24 @@ export class ImmunizationScheduleComponent implements OnInit {
     this.drugIndex = 0;
     this.immunizationScheduleForm = this._fb.group({
       name: ['', [<any>Validators.required]],
+      price: ['', [<any>Validators.required]],
       vaccines: this._fb.array([this.initVaccineBuilder()])
     });
 
+    const immuneSch = <FormArray>this.immunizationScheduleForm.controls['vaccines'];
+    const vaccine = <FormArray>immuneSch.controls[this.drugIndex];
+    const intervals = <FormArray>vaccine.controls['intervals'];
     (this.immunizationScheduleForm.get('vaccines') as FormArray).valueChanges
       .debounceTime(400)
       .distinctUntilChanged()
       // .switchMap(value => this._drugListAPI.find({ query: { 'text': value[this.drugIndex].name, facilityId: this.facility._id }}))
       .subscribe((res: any) => {
+        console.log(res);
         if (res.length > 0) {
           const text = res[this.drugIndex].name;
+          const intervalLength = intervals.length
           // Get the length from interval and assign numberOfDosage
-          const immuneSch = <FormArray>this.immunizationScheduleForm.controls['vaccines'];
-          const vaccine = <FormArray>immuneSch.controls[this.drugIndex];
-          const interval = <FormArray>vaccine.controls['intervals'].length;
-          vaccine.controls['numberOfDosage'].setValue(interval);
+          vaccine.controls['numberOfDosage'].setValue(intervalLength);
 
           this._drugListAPI.find({ query: { searchtext: text, method : 'immunization' } }).then(resp => {
             this.cuDropdownLoading = false;
@@ -68,17 +73,51 @@ export class ImmunizationScheduleComponent implements OnInit {
           });
         }
       });
+
+    // if (intervals.length > 0) {
+    //   (this.immunizationScheduleForm.get('intervals') as FormArray).valueChanges
+    //     .subscribe(res => {
+    //       console.log(res);
+    //     });
+    // }
   }
 
   onClickSaveSchedule(valid: boolean, value: any) {
     if (valid) {
-      console.log(value);
-      const payload = {
-        name: value.name,
-        vaccines: value.vaccines
-      }
+      this.disableBtn = true;
+      this.saveScheduleBtn = false;
+      this.updateScheduleBtn = false;
+      this.savingScheduleBtn = true;
+      this.updatingScheduleBtn = false;
 
+      const payload = {
+        action: 'create',
+        facilityId: this.facility._id,
+        name: value.name,
+        vaccines: value.vaccines,
+        price: value.price
+      };
       console.log(payload);
+      this._immunizationScheduleService.create(payload).then(res => {
+        console.log(res);
+        if (res.status === 'success') {
+          const text = `${value.name} immunization schedule has been created successfully!`;
+          this._systemModuleService.announceSweetProxy('success', text);
+          this.immunizationScheduleForm.reset();
+          this.disableBtn = false;
+          this.saveScheduleBtn = true;
+          this.updateScheduleBtn = false;
+          this.savingScheduleBtn = false;
+          this.updatingScheduleBtn = false;
+        } else {
+          this._systemModuleService.announceSweetProxy('success', res.message);
+          this.disableBtn = false;
+          this.saveScheduleBtn = true;
+          this.updateScheduleBtn = false;
+          this.savingScheduleBtn = false;
+          this.updatingScheduleBtn = false;
+        }
+      });
     }
   }
 
@@ -88,7 +127,7 @@ export class ImmunizationScheduleComponent implements OnInit {
       nameCode: ['', Validators.required], // drug code
       code: ['', Validators.required],
       vaccinationSite: ['', Validators.required],
-      numberOfDosage: ['', Validators.required],
+      numberOfDosage: [{ value: '', disabled: true }, Validators.required],
       dosage: ['', Validators.required],
       price: ['', Validators.required],
       intervals: this._fb.array([this.initIntervalBuilder()]),
@@ -103,6 +142,7 @@ export class ImmunizationScheduleComponent implements OnInit {
 
   initIntervalBuilder() {
     return this._fb.group({
+      sequence: ['', Validators.required],
       unit: ['', Validators.required],
       duration: ['', Validators.required],
     });
@@ -121,10 +161,15 @@ export class ImmunizationScheduleComponent implements OnInit {
     control.push(this.initVaccineBuilder());
   }
 
-  onClickAddInterval(i, j, vaccine) {
-    // add interval into the list of vaccines.
+  onClickAddInterval(i, vaccine, action) {
     const control = <FormArray>vaccine.controls['intervals'];
-    control.push(this.initIntervalBuilder());
+    if (action === 'add') {
+      // add interval into the list of vaccines.
+      control.push(this.initIntervalBuilder());
+    } else {
+      // Remove interval from the list of vaccines.
+      control.removeAt(control.length - 1);
+    }
   }
 
   onValueChangeResults(control) {
@@ -132,16 +177,11 @@ export class ImmunizationScheduleComponent implements OnInit {
   }
 
   apmisLookupHandleSelectedItem(i, value, vaccine) {
-    console.log(value);
-    console.log(vaccine);
     vaccine.controls['name'].setValue(value.name);
-    vaccine.controls['code'].setValue(value.name);
-    // this.selectedProcedure = value;
-    // this.addProcedureForm.controls['procedure'].setValue(value.name);
+    vaccine.controls['nameCode'].setValue(value.code);
   }
 
   focusSearch(i) {
-    console.log(i);
     this.drugIndex = i;
     this.showCuDropdown = !this.showCuDropdown;
   }
