a2a59772f3047dda3bb6f8ade0d179b132bbea3c
starday01 <sunday@apmis.ng>
2018-05-18 15:15:38 +0100

    Implemented validating duplicate person

--- src/services/save-person/save-person.class.js
+++ src/services/save-person/save-person.class.js
@@ -2,6 +2,8 @@
 const logger = require('winston');
 const sms = require('../../templates/sms-sender');
 const tokenLabel = require('../../parameters/token-label');
+const jsend = require('jsend');
+const errors = require('@feathersjs/errors');
 class Service {
     constructor(options) {
         this.options = options || {};
@@ -22,29 +24,45 @@ class Service {
         const userService = this.app.service('users');
         const personService = this.app.service('people');
         const getTokenService = this.app.service('get-tokens');
+        const searchPeople = this.app.service('search-people');
 
-        let person = data.person;
-        person.wallet = {
-            'transactions': [],
-            'ledgerBalance': 0,
-            'balance': 0
-        };
-        var createPerson = await personService.create(person);
-        const user = {
-            email: createPerson.apmisId,
-            personId: createPerson._id
-        };
+        let checkPerson = await searchPeople.find({
+            query: {
+                firstName: 'Sunday', //data.person.firstName,
+                dateOfBirth: data.person.dateOfBirth,
+                gender: data.person.gender,
+                motherMaidenName: data.person.motherMaidenName,
+                isValidating: true
+            }
+        });
+        if (checkPerson.status === 'success' && checkPerson.data === true) {
+            throw new Error('Duplicate record detected, please check and try again!');
+        } else {
+            let person = data.person;
+            person.wallet = {
+                'transactions': [],
+                'ledgerBalance': 0,
+                'balance': 0
+            };
 
-        var createToken = await getTokenService.get(tokenLabel.tokenType.autoPassword, {});
-        user.password = createToken.result;
-        if (data.facilityId !== undefined) {
-            user.facilitiesRole = [];
-            user.facilitiesRole.push({ facilityId: data.facilityId });
+            var createPerson = await personService.create(person);
+            const user = {
+                email: createPerson.apmisId,
+                personId: createPerson._id
+            };
+
+            var createToken = await getTokenService.get(tokenLabel.tokenType.autoPassword, {});
+            user.password = createToken.result;
+            if (data.facilityId !== undefined) {
+                user.facilitiesRole = [];
+                user.facilitiesRole.push({ facilityId: data.facilityId });
+            }
+
+            var createUser = await userService.create(user);
+            await sms.sendAutoGeneratorPassword(createPerson, createToken.result);
+            return createPerson;
         }
 
-        var createUser = await userService.create(user);
-        await sms.sendAutoGeneratorPassword(createPerson, createToken.result);
-        return createPerson;
     }
 
     update(id, data, params) {
