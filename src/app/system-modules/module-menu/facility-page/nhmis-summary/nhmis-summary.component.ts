e204e0bae6c2b769296a7e8b84e5dcbe25681598
aiyk <aiyk.ekwe@gmail.com>
2018-07-18 12:15:06 +0100

    nhmis

---
+++ src/app/system-modules/module-menu/facility-page/nhmis-summary/nhmis-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nhmis-summary',
+  templateUrl: './nhmis-summary.component.html',
+  styleUrls: ['./nhmis-summary.component.scss']
+})
+export class NhmisSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
