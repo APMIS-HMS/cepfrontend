8e924d6e1513113e17443c38b4587085c43e0a40
aiyk <aiyk.ekwe@gmail.com>
2018-07-19 12:43:35 +0100

    nhmis

--- src/app/system-modules/module-menu/facility-page/nhmis-summary/nhmis-summary.component.ts
+++ src/app/system-modules/module-menu/facility-page/nhmis-summary/nhmis-summary.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as jspdf from 'jspdf'; 
+import { html2canvas} from 'html2canvas';  
 
 @Component({
   selector: 'app-nhmis-summary',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NhmisSummaryComponent implements OnInit {
 
+  filterMonth: FormControl = new FormControl();
+  filterYear: FormControl = new FormControl();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  downloadPdf(){
+    var data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('NHMIS.pdf'); // Generated PDF   
+    });  
+   
   }
 
 }
