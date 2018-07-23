41da3d0a52250e4baa653f0ae923b8f0140971c1
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-07-23 10:01:28 +0100

    updates

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -10,8 +10,7 @@ import {Injectable} from '@angular/core';
 const rx = require('feathers-reactive');
 const RxJS = require('rxjs/Rx');
 
-const HOST = 'https://apmisapitest.azurewebsites.net';
-//  const HOST = 'http://localhost:3031';
+const HOST = 'https://apmisapilive.azurewebsites.net';
 
 @Injectable()
 export class SocketService {
