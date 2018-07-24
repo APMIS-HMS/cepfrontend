060c797802da9685b416568eb8bb7ff29beeab39
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-07-24 16:42:31 +0100

    updates

--- src/app/system-modules/module-menu/facility-page/nhmis-summary/nhmis-summary.component.ts
+++ src/app/system-modules/module-menu/facility-page/nhmis-summary/nhmis-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
@@ -13,16 +13,30 @@ export class NhmisSummaryComponent implements OnInit {
   filterMonth: FormControl = new FormControl();
   filterYear: FormControl = new FormControl();
   // dd = { content: document.getElementById('contentToConvert') };
-
+  @ViewChild('contentToConvert') input: ElementRef;
   constructor() { 
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    console.log(this.input);
   }
 
   ngOnInit() {
   }
-
-  generatePdf(){ 
-    pdfMake.createPdf({content: document.getElementById('contentToConvert')}).open();
+  ngAfterViewInit() {
+    console.log('ngafter');
+    console.log(this.input);
   }
+  generatePdf() { 
+    console.log(this.input.nativeElement.innerHTML);
+    try {
+      var docDefinition = { content: this.input.nativeElement.innerHTML }; 
+      pdfMake.createPdf(docDefinition).open();
+      console.log('after');
+    } catch (error) {
+      console.log(error);
+    }
+   
+    // pdfMake.createPdf({content: document.getElementById('contentToConvert')}).open();
+  }
+  
 
 }
