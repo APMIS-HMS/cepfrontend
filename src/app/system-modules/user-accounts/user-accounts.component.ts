1187a27178191d5765c468cf1a95004511c2190d
farquardsolve <fakuadesegun@live.com>
2018-09-14 12:29:40 +0100

    update

--- src/app/system-modules/user-accounts/user-accounts.component.ts
+++ src/app/system-modules/user-accounts/user-accounts.component.ts
@@ -24,7 +24,7 @@ export class UserAccountsComponent implements OnInit {
   selectedPerson: Person = <Person>{};
   logoutConfirm_on = false;
   loginEmployee: Employee = <Employee>{};
-  authData: any; 
+  authData: any;
   constructor(private locker: CoolLocalStorage,
     private router: Router,
     private route: ActivatedRoute,
@@ -33,7 +33,7 @@ export class UserAccountsComponent implements OnInit {
     private sanitizer: DomSanitizer,
     private corporateFacilityService: CorporateFacilityService,
     private employeeService: EmployeeService,
-    private joinChannelService:JoinChannelService,
+    private joinChannelService: JoinChannelService,
     public facilityService: FacilitiesService) {
     this.userService.missionAnnounced$.subscribe(payload => {
       if (payload === 'out') {
@@ -90,22 +90,26 @@ export class UserAccountsComponent implements OnInit {
   popListing(item: any) {
     const auth: any = this.locker.getObject('auth');
     this.item = item;
-    this.facilityService.get(item._id,{}).then(payload =>{
+    this.facilityService.get(item._id, {}).then(payload => {
       this.selectedFacility = payload;
       if (this.selectedFacility.isTokenVerified === false) {
         this.popup_verifyToken = true;
         this.popup_listing = false;
       } else {
-        this.joinChannelService.create({_id:this.selectedFacility._id, userId:auth.data._id}).then(pay =>{
+        let dataChannel = {
+          '_id': this.selectedFacility._id,
+          'userId': auth.data._id,
+          'dept': this.selectedFacility.departments,
+          'facilityName': this.selectedFacility.name
+        }
+        this.joinChannelService.create(dataChannel).then(pay => {
           this.popup_listing = true;
           this.popup_verifyToken = false;
-        }, err =>{
-          
-        })
-        
+        }, err => {
+        });
       }
       this.locker.setObject('selectedFacility', this.selectedFacility);
-      this.locker.setObject('fac',this.selectedFacility._id);
+      this.locker.setObject('fac', this.selectedFacility._id);
       this.logoutConfirm_on = false;
     })
 
@@ -115,10 +119,10 @@ export class UserAccountsComponent implements OnInit {
     this.logoutConfirm_on = false;
     this.popup_verifyToken = false;
   }
-  close_onClick2(){
+  close_onClick2() {
     this.popup_verifyToken = false;
-    if(this.item !== undefined && this.item._id !== undefined){
-      this.popListing(this.item); 
+    if (this.item !== undefined && this.item._id !== undefined) {
+      this.popListing(this.item);
     }
   }
   logoutConfirm_show() {
