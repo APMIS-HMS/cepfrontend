f9f37e2bcd95a9e28c4d5b6dd06870075838e626
aiyk <aiyk.ekwe@gmail.com>
2018-09-14 01:20:41 +0100

    new radiology recoppied

--- src/app/system-modules/dashboard/dashboard-routes.ts
+++ src/app/system-modules/dashboard/dashboard-routes.ts
@@ -122,11 +122,11 @@ const appRoutes: Routes = [
                 loadChildren: '../module-menu/lab/lab.module#LabModule',
                 // canActivate: [
                 //     CanActivateViaAuthGuardAccessService
-                // ]
+                // ] 
             },
             {
              path: 'radiology',
-             loadChildren: '../module-menu/radiology/radiology.module#RadiologyModule',
+             loadChildren: '../module-menu/new-radiology/radiology.module#RadiologyModule',
             //  loadChildren: '../module-menu/radiology-module/radiology-module.module#RadiologyModuleModule',
             //  canActivate: [
             //     CanActivateViaAuthGuardAccessService
