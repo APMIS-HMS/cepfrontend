e35528bfbaeecf958073a4bb4dda842c017c9db7
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-10-02 11:56:13 +0100

    updates

--- src/app/models/facility-manager/setup/prescription-item.ts
+++ src/app/models/facility-manager/setup/prescription-item.ts
@@ -1,52 +1,52 @@
 export interface PrescriptionItem {
-    billId?: string;
-    billItemId?: string;
-    _id?: string;
-    facilityId?: string;
-    productName?: any;
-    productId?: string;
-    genericName: string;
-    ingredients?: any[];
-    regimen: any[];
-    form?: string;
-    frequency?: string;
-    duration?: string;
-    dosage?: string;
-    cost?: number;
-    totalCost?: number;
-    routeName: String;
-    quantity?: number;
-    quantityDispensed?: number;
-    dispensedBalance?: number;
-    dispensed?: Dispensed;
-    paymentCompleted?: boolean;
-    patientInstruction: string;
-    isRefill?: Boolean;
-    isExternal: Boolean;
-    refillCount: number;
-    initiateBill: boolean;
-    isBilled: boolean;
-    isDispensed: boolean;
-    isOpen?: boolean;
-    transactions?: any[];
-    serviceId?: string;
-    facilityServiceId?: string;
-    categoryId?: string;
+	billId?: string;
+	billItemId?: string;
+	_id?: string;
+	facilityId?: string;
+	productName?: any;
+	productId?: string;
+	genericName: string;
+	ingredients?: any[];
+	regimen?: any[];
+	form?: string;
+	frequency?: string;
+	duration?: string;
+	dosage?: string;
+	cost?: number;
+	totalCost?: number;
+	routeName: String;
+	quantity?: number;
+	quantityDispensed?: number;
+	dispensedBalance?: number;
+	dispensed?: Dispensed;
+	paymentCompleted?: boolean;
+	patientInstruction: string;
+	isRefill?: Boolean;
+	isExternal: Boolean;
+	refillCount: number;
+	initiateBill: boolean;
+	isBilled: boolean;
+	isDispensed: boolean;
+	isOpen?: boolean;
+	transactions?: any[];
+	serviceId?: string;
+	facilityServiceId?: string;
+	categoryId?: string;
 }
 
 export interface Dispensed {
-    totalQtyDispensed: number;
+	totalQtyDispensed: number;
 	outstandingBalance: number;
 	dispensedArray?: DispensedArray[];
 }
 
 export interface DispensedArray {
-    orderIndex: number; // unique
-    dispensedDate: Date; // Date time
-    batchNumber: String;
-    qty: number;
-    employee: any;
-    store: any;
-    unitBilledPrice: number;
-    totalAmount: number;
+	orderIndex: number; // unique
+	dispensedDate: Date; // Date time
+	batchNumber: String;
+	qty: number;
+	employee: any;
+	store: any;
+	unitBilledPrice: number;
+	totalAmount: number;
 }
