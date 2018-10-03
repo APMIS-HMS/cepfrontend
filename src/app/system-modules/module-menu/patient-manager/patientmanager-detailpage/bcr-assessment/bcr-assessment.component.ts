e905c2acb698fab6670a8c9a40f6eb423f2136eb
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-10-03 17:04:59 +0100

    updades

--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/bcr-assessment/bcr-assessment.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/bcr-assessment/bcr-assessment.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bcr-assessment',
-  templateUrl: './bcr-assessment.component.html',
-  styleUrls: ['./bcr-assessment.component.scss', '../../../payment/bill-lookup/bill-lookup.component.scss']
+	selector: 'app-bcr-assessment',
+	templateUrl: './bcr-assessment.component.html',
+	styleUrls: [
+		'./bcr-assessment.component.scss',
+		'../../../payment/bill-lookup/bill-lookup.component.scss',
+	],
 })
 export class BcrAssessmentComponent implements OnInit {
-  patient: any;
-  selectedDocument:any;
-  docDetail_view = false;
-  addProblem_view = false;
-  addAllergy_view = false;
-  addHistory_view = false;
-  addVitals_view = false;
-  constructor() { }
+	patient: any;
+	selectedDocument: any;
+	docDetail_view = false;
+	addProblem_view = false;
+	addAllergy_view = false;
+	addHistory_view = false;
+	addVitals_view = false;
+	constructor() {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
-  addProblem_show(event) {}
+	addProblem_show(event) {}
 
-  addHistory_show(event) {}
+	addHistory_show(event) {}
 
-  addVitals_show(event) {}
+	addVitals_show(event) {}
 
-  addAllergy_show(event) {}
-
-  close_onClick(event){}
+	addAllergy_show(event) {}
+	close_onClick(event){}
 }
-
