01edf634213fe9b21abaa5319e49361b4b5b36a4
Chisimdi Damian Ezeanieto <chisimdi.ezeanieto@apmis.ng>
2018-10-03 16:49:01 +0100

    Started working on Breast Cancer.

--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/bcr-assessment/bcr-assessment.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/bcr-assessment/bcr-assessment.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-bcr-assessment',
   templateUrl: './bcr-assessment.component.html',
   styleUrls: ['./bcr-assessment.component.scss', '../../../payment/bill-lookup/bill-lookup.component.scss']
 })
 export class BcrAssessmentComponent implements OnInit {
+  bcrFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.bcrFormGroup = this._fb.group({
+      Asymptomatic: ['', Validators.required],
+      hivCounselling: ['', Validators.required],
+      personalHistoryOfBreatCancer: ['', Validators.required],
+      mammogram: ['', Validators.required],
+      historyOfBreastImplant: ['', Validators.required],
+      firstDegreeeHasGeneMutation: ['', Validators.required],
+      firstDegreeeHasGeneticCounsellingAndTesting: ['', Validators.required],
+      twoOrMoreCasesOfBreastCancerOrOvarianCancer: ['', Validators.required],
+      bilateralBreastCancer: ['', Validators.required],
+      bothBreastAndOvarianInTheSameWoman: ['', Validators.required],
+      seriousInvasiveSeriousOvarian: ['', Validators.required],
+      breastOrOvarianCancerInAshkenaziJewishFamily: ['', Validators.required],
+      indentifiedGeneMutationInAnyBloodRelative: ['', Validators.required],
+      maleBreastCancer: ['', Validators.required],
+      knownCarrierOfAGeneMutation: ['', Validators.required],
+      firstDegreeHasHadGeneticCounsellingAndHasDeclinedTesting: ['', Validators.required],
+      previouslyAssessedAsHavingAGreaterLifetimeRiskOfBreastCancer: ['', Validators.required],
+      receiveChestXRayBeforeAge30AndAtLeast8YrsPreviously: ['', Validators.required],
+    });
   }
 
 }
