158076da42a168878adebde128a7477fb0cfb201
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-10-17 16:57:21 +0100

    updates

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -7,12 +7,18 @@ const rest = require('@feathersjs/rest-client');
 const authentication = require('@feathersjs/authentication-client');
 import { CoolLocalStorage } from 'angular2-cool-storage';
 import { Injectable } from '@angular/core';
-import { API_LOCALHOST, API_DEV, API_TEST, API_LIVE } from '../shared-module/helpers/global-config';
+import {
+	API_LOCALHOST,
+	API_DEV,
+	API_TEST,
+	API_LIVE,
+	USE_LOGIN_ENCRYPTION
+} from '../shared-module/helpers/global-config';
 const rx = require('feathers-reactive');
 const RxJS = require('rxjs/Rx');
 const CircularJSON = require('circular-json');
 
-const HOST = API_TEST;
+const HOST = API_LOCALHOST;
 
 @Injectable()
 export class SocketService {
@@ -72,11 +78,19 @@ export class RestService {
 		}
 	}
 	loginIntoApp(query) {
-		return this._app.authenticate({
-			strategy: 'local',
-			email: CircularJSON.stringify(query.email),
-			password: CircularJSON.stringify(query.password)
-		});
+		if (!USE_LOGIN_ENCRYPTION) {
+			return this._app.authenticate({
+				strategy: 'local',
+				email: query.email,
+				password: query.password
+			});
+		} else {
+			return this._app.authenticate({
+				strategy: 'local',
+				email: CircularJSON.stringify(query.email),
+				password: CircularJSON.stringify(query.password)
+			});
+		}
 	}
 	getService(value: any) {
 		// this._app.authenticate();
