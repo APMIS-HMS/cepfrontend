0716a1cb344a60fa738460c22919d4a316d98157
kemijibzz <oluwakemi.awosile@apmis.ng>
2018-10-17 09:05:54 +0100

    no billing for ward #39 fixed

--- src/app/shared-module/pipes/days-in-dates.ts
+++ src/app/shared-module/pipes/days-in-dates.ts
@@ -1,19 +1,14 @@
 import {Pipe, Injectable, PipeTransform } from '@angular/core';
+import * as differenceInCalendarDays from 'date-fns/difference_in_calendar_days';
 
 @Pipe({
-    name: 'daysInDate'
+    name: 'daysInCalenderDate'
   })
 
 @Injectable()
 export class DaysInDates implements PipeTransform {
 
     transform(startDate: string, endDate: string) {
-        const startDateToDate: any = new Date(startDate);
-        const endDateToDate: any = new Date(endDate);
-        console.log(`start date is ${startDateToDate}, end date is ${endDateToDate}`);
-        const differenceInDate = (endDateToDate - startDateToDate) / 1000 / 60 / 60 / 24;
-        const numberInDays = Math.round(differenceInDate);
-        console.log(numberInDays);
-        return numberInDays;
+        return differenceInCalendarDays(endDate, startDate);
     }
 }
