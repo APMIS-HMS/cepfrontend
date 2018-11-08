5bd9d547b87bb57bac3aafb3c22a37a474f2efb7
sayrai <omoikesarah@gmail.com>
2018-11-08 13:46:25 +0100

    report routing

--- src/app/system-modules/module-menu/reports/report-dashboard/report-dashboard-routing.module.ts
+++ src/app/system-modules/module-menu/reports/report-dashboard/report-dashboard-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReportDashboardComponent } from "./report-dashboard.component";
 import { RegisterEntriesComponent } from "./../registers/register-entries/register-entries.component";
 import { DhisReportComponent } from "./../dhis-report/dhis-report.component";
-
+import { LaboratoryReportComponent } from './laboratory-report/laboratory-report.component';
+import { PatientManagerReportComponent } from './patient-manager-report/patient-manager-report.component';
 
 const routes: Routes = [
-    { path:'', redirectTo: 'report'},
+    // { path:'', redirectTo: 'report'},
     {path: '', component: ReportDashboardComponent },    
     { path: 'register-entries', component: RegisterEntriesComponent },
-    { path: 'dhis-report', component: DhisReportComponent }
+    { path: 'dhis-report', component: DhisReportComponent },
+    { path: 'laboratory-report', component: LaboratoryReportComponent },
+    { path: 'patient-report', component: PatientManagerReportComponent }
 ];
 
 @NgModule({
