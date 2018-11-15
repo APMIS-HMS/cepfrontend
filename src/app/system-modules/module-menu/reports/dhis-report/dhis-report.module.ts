2bb925afbd9a124deb4b9d4b775da2102bc07b80
sayrai <omoikesarah@gmail.com>
2018-11-15 11:18:13 +0100

    patient report setup dashboard back link searchControl

--- src/app/system-modules/module-menu/reports/dhis-report/dhis-report.module.ts
+++ src/app/system-modules/module-menu/reports/dhis-report/dhis-report.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../../../shared-module/shared.module';
 import { OnlyMaterialModule } from '../../../../shared-common-modules/only-material-module';
 import { MaterialModule } from '../../../../shared-common-modules/material-module';
-import { RegistersComponent } from './registers/registers.component';
-import { NhmisSummaryComponent } from './nhmis-summary/nhmis-summary.component';
+import { RegistersComponent } from '../report-dashboard/dhis-report/registers/registers.component';
+import { NhmisSummaryComponent } from '../report-dashboard/dhis-report/nhmis-summary/nhmis-summary.component';
 
 
 @NgModule({
--- src/app/system-modules/module-menu/reports/report-dashboard/patient-manager-report/patient-manager-report.component.ts
+++ src/app/system-modules/module-menu/reports/report-dashboard/patient-manager-report/patient-manager-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-patient-manager-report',
   templateUrl: './patient-manager-report.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientManagerReportComponent implements OnInit {
 
-  constructor() { }
+  searchControl = new FormControl();
+  searchCriteria = new FormControl('Search');
+
+  pageInView = 'Patient Registration Report';
+
+  constructor(private _router: Router) { }
 
   ngOnInit() {
   }
+
+  pageInViewLoader(title) {
+		this.pageInView = title;
+  }
+  back_dashboard() {
+		this._router.navigate(['/dashboard/reports/report-dashboard']);
+	  }
 
 }
