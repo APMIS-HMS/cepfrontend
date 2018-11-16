f1e8a0618f016cb2b99f8e58e45fb234458058f0
sayrai <omoikesarah@gmail.com>
2018-11-16 14:53:47 +0100

    patient registration route

--- src/app/system-modules/module-menu/reports/report-dashboard/patient-manager-report/patient-manager-report-routing.module.ts
+++ src/app/system-modules/module-menu/reports/report-dashboard/patient-manager-report/patient-manager-report-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PatientManagerReportComponent } from './patient-manager-report.component';
+import { PatientRegistrationAnalyticsComponent } from './patient-registration-analytics/patient-registration-analytics.component';
 
-const routes: Routes = [];
+const PATIENT_MANAGER_REPORT_ROUTE: Routes = [
+{
+  path: '',
+  component: PatientManagerReportComponent,
+  children: [
+    // { path: '', redirectTo: 'clinic-report-page' },
+    // { path: 'clinic-report-page', component: ClinicManagementReportComponent },
+    { path: 'patient-registration-analytics', component: PatientRegistrationAnalyticsComponent },
+  ]
+}
+];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class PatientManagerReportRoutingModule { }
+export const PatientManagerReportRoutingModule = RouterModule.forChild(PATIENT_MANAGER_REPORT_ROUTE);
