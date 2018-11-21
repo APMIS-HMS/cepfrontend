6b27d86920e5e9f74b2034c02e738ff829b79511
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-11-21 17:38:40 +0100

    updates to diagnosis in documentation

--- src/app/core-ui-modules/ui-components/report-generator-service.ts
+++ src/app/core-ui-modules/ui-components/report-generator-service.ts
@@ -1,114 +1,94 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { IGroupableLabReportModel, ILabReportModel, ILabReportOption, ILabReportSummaryModel } from './LabReportModel';
+import { RestService } from '../../feathers/feathers.service';
+import { IApiResponse, ICustomReportService, IPaymentReportServiceEndPoint } from './ReportGenContracts';
+import { IPatientReportModel, IPatientReportOptions } from './PatientReportModel';
 import {
-    IGroupableLabReportModel,
-    ILabReportModel,
-    ILabReportOption, ILabReportSummaryModel
-} from "./LabReportModel";
-import {RestService} from "../../feathers/feathers.service";
-import {IApiResponse, ICustomReportService, IPaymentReportServiceEndPoint} from "./ReportGenContracts";
-import {IPatientReportModel, IPatientReportOptions} from "./PatientReportModel";
-import {
-    IPaymentGroups,
-    IPaymentReportModel,
-    IPaymentReportOptions,
-    IPaymentReportSummaryModel
-} from "./PaymentReportModel";
-import {CoolLocalStorage} from "angular2-cool-storage";
-
+	IPaymentGroups,
+	IPaymentReportModel,
+	IPaymentReportOptions,
+	IPaymentReportSummaryModel
+} from './PaymentReportModel';
+import { CoolLocalStorage } from 'angular2-cool-storage';
 
 @Injectable()
 export class ReportGeneratorService implements ICustomReportService {
-    selectedFacility : any;
-    constructor(private restEndpoint: RestService, localStorage  :  CoolLocalStorage) {
-        //super();
-        this.selectedFacility  = localStorage.getObject("selectedFacility");
-        
-    }
+	selectedFacility: any;
+	constructor(private restEndpoint: RestService, localStorage: CoolLocalStorage) {
+		//super();
+		this.selectedFacility = localStorage.getObject('selectedFacility');
+	}
 
-    getLabReport(options: ILabReportOption): Promise<IApiResponse<ILabReportModel[]>> {
-        options.facilityId  = this.selectedFacility._id;
-        console.log(options,"OPTIONS");
-        return this.restEndpoint.getService("laboratory-report-summary")
-            .find({params: options});
-    }
+	getLabReport(options: ILabReportOption): Promise<IApiResponse<ILabReportModel[]>> {
+		options.facilityId = this.selectedFacility._id;
+		console.log(options, 'OPTIONS');
+		return this.restEndpoint.getService('laboratory-report-summary').find({ query: options });
+	}
 
-    getGroupedLabReport(options: ILabReportOption): Promise<IApiResponse<IGroupableLabReportModel[]>> {
-        options.facilityId  = this.selectedFacility._id;
-        return this.restEndpoint.getService("grouped-lab-report-service")
-            .find({params: options});
+	getGroupedLabReport(options: ILabReportOption): Promise<IApiResponse<IGroupableLabReportModel[]>> {
+		options.facilityId = this.selectedFacility._id;
+		return this.restEndpoint.getService('grouped-lab-report-service').find({ params: options });
+	}
 
-    }
+	getLabReportInvestigationSummary(options?: ILabReportOption): Promise<IApiResponse<ILabReportSummaryModel[]>> {
+		options.facilityId = this.selectedFacility._id;
+		return this.restEndpoint.getService('laboratory-report-summary').find({ params: options });
+	}
 
-    getLabReportInvestigationSummary(options?: ILabReportOption): Promise<IApiResponse<ILabReportSummaryModel[]>> {
-        options.facilityId  = this.selectedFacility._id;
-        return this.restEndpoint.getService("laboratory-report-summary")
-            .find({params: options});
-    }
+	getPatientReport(options: IPatientReportOptions): Promise<IApiResponse<IPatientReportModel[]>> {
+		return Promise.resolve({ data: [] });
+	}
 
-    getPatientReport(options: IPatientReportOptions): Promise<IApiResponse<IPatientReportModel[]>> {
-        return Promise.resolve({data: []});
-    }
-    
-    getWorkBenches() :any
-    {
-        // get the Service end point;
-       return this.restEndpoint.getService("workbenches").find( {
-            query : {
-                facilityID  : this.selectedFacility._id
-            }
-        });
-    }
-    getLocations()
-    {
-        return this.restEndpoint.getService("locations").find( {
-            query : {
-                facilityID  : this.selectedFacility._id
-            }
-        });
-    }
+	getWorkBenches(): any {
+		// get the Service end point;
+		return this.restEndpoint.getService('workbenches').find({
+			query: {
+				facilityID: this.selectedFacility._id
+			}
+		});
+	}
+	getLocations() {
+		return this.restEndpoint.getService('locations').find({
+			query: {
+				facilityID: this.selectedFacility._id
+			}
+		});
+	}
 
-    /*  
+	/*  
     *   Reports for Patients
     * */
-
 }
 @Injectable()
 export class PaymentReportGenerator implements IPaymentReportServiceEndPoint {
-    paymentReportServiceRef;
-    selectedFacility  :any ;
-    constructor(private restEndpoint: RestService, locker: CoolLocalStorage) {
-        //super();
-        this.paymentReportServiceRef = this.restEndpoint.getService("payment-reports");
-        this.selectedFacility = locker.getObject("selectedFacility");
-    }
+	paymentReportServiceRef;
+	selectedFacility: any;
+	constructor(private restEndpoint: RestService, locker: CoolLocalStorage) {
+		//super();
+		this.paymentReportServiceRef = this.restEndpoint.getService('payment-reports');
+		this.selectedFacility = locker.getObject('selectedFacility');
+	}
 
-    getInvoicePaymentReport(rptOption: IPaymentReportOptions): Promise<IApiResponse<IPaymentReportModel[]>> {
-        rptOption.facilityId  = this.selectedFacility._id;
-        return  this.paymentReportServiceRef.get(rptOption.facilityId,
-            {
-                query: rptOption
-            }
-        );
-       
-    }
+	getInvoicePaymentReport(rptOption: IPaymentReportOptions): Promise<IApiResponse<IPaymentReportModel[]>> {
+		rptOption.facilityId = this.selectedFacility._id;
+		return this.paymentReportServiceRef.get(rptOption.facilityId, {
+			query: rptOption
+		});
+	}
 
-    getPaymentGroups(rptOption: IPaymentReportOptions): Promise<IApiResponse<IPaymentGroups[]>> {
-        rptOption.facilityId  = this.selectedFacility._id;
-        return undefined;
-    }
+	getPaymentGroups(rptOption: IPaymentReportOptions): Promise<IApiResponse<IPaymentGroups[]>> {
+		rptOption.facilityId = this.selectedFacility._id;
+		return undefined;
+	}
 
-    getPaymentReportSummary(rptOption: IPaymentReportOptions): Promise<IApiResponse<IPaymentReportSummaryModel>> {
-        rptOption.facilityId  = this.selectedFacility._id;
+	getPaymentReportSummary(rptOption: IPaymentReportOptions): Promise<IApiResponse<IPaymentReportSummaryModel>> {
+		rptOption.facilityId = this.selectedFacility._id;
 
-        /* So to get  all report summary we simply send a 'isSummary' variable to the server, Date range might be checked on server*/
-        // we also expect the facility id to be set
-        rptOption.isSummary = true;
-       return  this.paymentReportServiceRef.get(rptOption.facilityId,
-            {
-                query: rptOption
-            }
-        );
-       
-    }
-
+		/* So to get  all report summary we simply send a 'isSummary' variable to the server, Date range might be checked on server*/
+		// we also expect the facility id to be set
+		rptOption.isSummary = true;
+		return this.paymentReportServiceRef.get(rptOption.facilityId, {
+			query: rptOption
+		});
+	}
 }
--- src/app/system-modules/module-menu/reports/report-dashboard/laboratory-report/laboratory-report.component.ts
+++ src/app/system-modules/module-menu/reports/report-dashboard/laboratory-report/laboratory-report.component.ts
@@ -1,92 +1,84 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {FormControl} from '@angular/forms';
-import {ReportGeneratorService} from "../../../../../core-ui-modules/ui-components/report-generator-service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { ReportGeneratorService } from '../../../../../core-ui-modules/ui-components/report-generator-service';
 import {
-    ILabReportModel,
-    ILabReportOption,
-    ILabReportSummaryModel
-} from "../../../../../core-ui-modules/ui-components/LabReportModel";
-import {IPagerSource} from "../../../../../core-ui-modules/ui-components/PagerComponent";
-import {IDateRange} from "ng-pick-daterange";
+	ILabReportModel,
+	ILabReportOption,
+	ILabReportSummaryModel
+} from '../../../../../core-ui-modules/ui-components/LabReportModel';
+import { IPagerSource } from '../../../../../core-ui-modules/ui-components/PagerComponent';
+import { IDateRange } from 'ng-pick-daterange';
 
 @Component({
-    selector: 'app-laboratory-report',
-    templateUrl: './laboratory-report.component.html',
-    styleUrls: ['./laboratory-report.component.scss']
+	selector: 'app-laboratory-report',
+	templateUrl: './laboratory-report.component.html',
+	styleUrls: [ './laboratory-report.component.scss' ]
 })
 export class LaboratoryReportComponent implements OnInit {
+	searchControl = new FormControl();
+	searchCriteria = new FormControl('Search');
 
-    searchControl = new FormControl();
-    searchCriteria = new FormControl('Search');
+	pageInView = 'Laboratory Report';
+	processing: boolean = false;
+	reportData: ILabReportModel[] = [];
+	reportOptions: ILabReportOption = {
+		filterByDate: true,
+		startDate: new Date(2018, 7, 20, 0, 30, 10),
+		endDate: new Date()
+	};
+	reportSummaryData: ILabReportSummaryModel[] = [];
+	pagerSource: IPagerSource = { totalPages: 0, totalRecord: 0, currentPage: 0, pageSize: 30 };
+	constructor(private _router: Router, private reportSource: ReportGeneratorService) {}
 
-    pageInView = 'Laboratory Report';
-    processing: boolean = false;
-    reportData: ILabReportModel[] = [];
-    reportOptions: ILabReportOption = {
-        filterByDate: true,
-        startDate: new Date(2018, 7, 20, 0, 30, 10),
-        endDate: new Date()
-    }
-    reportSummaryData: ILabReportSummaryModel[] = [];
-    pagerSource : IPagerSource = { totalPages : 0 , totalRecord : 0 , currentPage : 0 , pageSize : 30};
-    constructor(private _router: Router, private reportSource: ReportGeneratorService) {
-    }
+	ngOnInit() {
+		this.getReportData();
+		this.getReportSummary();
+	}
 
-    ngOnInit() {
-        this.getReportData();
-        this.getReportSummary();
-    }
+	getReportData() {
+		this.processing = true;
+		if (this.reportOptions.paginate) {
+			this.reportOptions.paginationOptions = {
+				limit: this.pagerSource.pageSize,
+				skip: this.pagerSource.currentPage * this.pagerSource.pageSize
+			};
+		}
+		this.reportSource.getLabReport(this.reportOptions).then((x: any) => {
+			console.log(x);
+			this.reportData = x.data.data;
+			this.processing = false;
+			this.pagerSource.totalRecord = x.data.total;
+		});
+	}
 
-    getReportData() {
-        this.processing = true;
-        if(this.reportOptions.paginate)
-        {
-            this.reportOptions.paginationOptions = {
-                limit  : this.pagerSource.pageSize,
-                skip : this.pagerSource.currentPage * this.pagerSource.pageSize
-            }
-        }
-        this.reportSource.getLabReport(this.reportOptions)
-            .then(x => {
-                console.log(x);
-                this.reportData = x.data;
-                this.processing = false;
-                this.pagerSource.totalRecord  = x.total;
-            });
-    }
+	getReportSummary() {
+		this.reportSource.getLabReportInvestigationSummary(this.reportOptions).then((x) => {
+			console.log(x);
+			this.reportSummaryData = x.data;
+		});
+		console.log(this.reportOptions);
+	}
 
-    getReportSummary() {
-        this.reportSource.getLabReportInvestigationSummary(this.reportOptions)
-            .then(x => {
-                console.log(x);
-                this.reportSummaryData = x.data;
-            });
-        console.log(this.reportOptions);
-    }
+	pageInViewLoader(title) {
+		this.pageInView = title;
+	}
 
-    pageInViewLoader(title) {
-        this.pageInView = title;
-    }
+	back_dashboard() {
+		this._router.navigate([ '/dashboard/reports/report-dashboard' ]);
+	}
+	pageClickedEvent(index: number) {
+		// goto next page using the current index
+		this.pagerSource.currentPage = index;
+		this.getReportData();
+	}
+	assignDate(date: IDateRange) {
+		this.reportOptions.startDate = date.from;
+		this.reportOptions.endDate = date.to;
+	}
 
-    back_dashboard() {
-        this._router.navigate(['/dashboard/reports/report-dashboard']);
-    }
-    pageClickedEvent(index: number) {
-        // goto next page using the current index
-        this.pagerSource.currentPage = index;
-        this.getReportData();
-    }
-    assignDate(date:IDateRange)
-    {
-        this.reportOptions.startDate  = date.from;
-        this.reportOptions.endDate  = date.to;
-    }
-    
-    search(queryString  : string)
-    {
-        this.reportOptions.queryString  = queryString;
-        // this.reportOptions.searchBy =
-    }
-
+	search(queryString: string) {
+		this.reportOptions.queryString = queryString;
+		// this.reportOptions.searchBy =
+	}
 }
