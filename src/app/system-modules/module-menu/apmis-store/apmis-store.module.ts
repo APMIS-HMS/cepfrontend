85500e6916c9f9e442033c033d7805237108df05
aiyk <aiyk.ekwe@gmail.com>
2018-11-21 16:27:52 +0100

    all products

--- src/app/system-modules/module-menu/apmis-store/apmis-store.module.ts
+++ src/app/system-modules/module-menu/apmis-store/apmis-store.module.ts
@@ -5,12 +5,14 @@ import { ApmisStoreRoutingModule } from './apmis-store-routing.module';
 import { ApmisStoreLandingpageComponent } from './apmis-store-landingpage.component';
 import { ApmisStoreHomeComponent } from './apmis-store-home/apmis-store-home.component';
 import { StoreHomeAnalyticsComponent } from './apmis-store-home/store-home-analytics/store-home-analytics.component';
+import { StoreHomeMainComponent } from './apmis-store-home/store-home-main/store-home-main.component';
+import { AllProductsComponent } from './apmis-store-home/store-home-main/all-products/all-products.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ApmisStoreRoutingModule
   ],
-  declarations: [ApmisStoreLandingpageComponent, ApmisStoreHomeComponent, StoreHomeAnalyticsComponent]
+  declarations: [ApmisStoreLandingpageComponent, ApmisStoreHomeComponent, StoreHomeAnalyticsComponent, StoreHomeMainComponent, AllProductsComponent]
 })
 export class ApmisStoreModule { }
