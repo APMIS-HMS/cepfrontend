401863b18123d0a283dc5001aa348974a735fd02
aiyk <aiyk.ekwe@gmail.com>
2018-11-22 17:10:19 +0100

    store landing page

--- src/app/system-modules/module-menu/apmis-store/apmis-store-home/store-home-main/all-products/all-products.component.ts
+++ src/app/system-modules/module-menu/apmis-store/apmis-store-home/store-home-main/all-products/all-products.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProductsComponent implements OnInit {
 
+  clickItemIndex: number;
+  expand_row = false;
+  people: any[] = [
+    {firstname: "Ikechukwu"},
+    {firstname: "Ikechukwu"},
+    {firstname: "Ikechukwu"},
+    {firstname: "Ikechukwu"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  item_to_show(i) {
+    if(this.expand_row){
+      return this.clickItemIndex === i; 
+    }
+  } 
+  toggle_tr(itemIndex){
+    this.clickItemIndex = itemIndex;
+    this.expand_row = !this.expand_row;
   }
 
 }
