fb7540b9c3c7d298b71112624a8ee54d63dcda0f
kemijibzz <oluwakemi.awosile@apmis.ng>
2018-11-23 10:53:44 +0100

    pulled from development

--- src/app/services/reports/clinic-manager/diagnosis-report.service.ts
+++ src/app/services/reports/clinic-manager/diagnosis-report.service.ts
@@ -10,39 +10,9 @@ export class DiagnosisReportService {
     constructor(private socketService: SocketService, private restService: RestService) {
         this._socket = this.socketService.getService('appointment-diagnosis-report');
         this._rest = this.socketService.getService('appointment-disgnosis-report');
-        // this.diagnosis = [
-        //     {
-        //         hospitalNumber: 'LT/777/108A',
-        //         sex: 'Female',
-        //         age: 18,
-        //         diagnosisCode: 'Mal/8739',
-        //         clinic: 'Cardiology'
-        //     },
-        //     {
-        //         hospitalNumber: 'LT/877/107A',
-        //         sex: 'Male',
-        //         age: 108,
-        //         diagnosisCode: 'Thy/8739',
-        //         clinic: 'Radiology'
-        //     },
-        //     {
-        //         hospitalNumber: 'LT/877/107A',
-        //         sex: 'Male',
-        //         age: 30,
-        //         diagnosisCode: 'Mal/8739',
-        //         clinic: 'Cardiology'
-        //     }
-        // ];
     }
 
     find(query) {
-        // const diagnosisPromise = new Promise((resolve) => {
-        //     setTimeout(() => {
-        //         resolve(this.diagnosis);
-        //     }, 1000);
-        // });
-        // return diagnosisPromise;
-
          return this._socket.find(query);
     }
     get(query) {
