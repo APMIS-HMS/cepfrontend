4e00ef9154fb33a618465d0f2a33bdcd3160b3fc
sayrai <omoikesarah@gmail.com>
2018-11-29 20:36:26 +0100

    --updates

--- src/app/system-modules/module-menu/reports/reports.module.ts
+++ src/app/system-modules/module-menu/reports/reports.module.ts
@@ -3,10 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports.component';
-import {
-	WardMan
-	agerReportComponent
-} from './report-dashboard/ward-manager-report/ward-manager-report.component';
+import { WardManagerReportComponent } from './report-dashboard/ward-manager-report/ward-manager-report.component';
 import { StoreManagerReportComponent } from './report-dashboard/store-manager-report/store-manager-report.component';
 
 @NgModule({
