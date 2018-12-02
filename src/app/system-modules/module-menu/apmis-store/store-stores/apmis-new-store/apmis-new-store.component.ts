2d3fd9fccf5bfe20c655da00048bf5bf64be6b92
farquardsolve <fakuadesegun@live.com>
2018-12-02 21:01:33 +0100

    update@completed  apmis-filter and badge tool component and also function to save new store

--- src/app/system-modules/module-menu/apmis-store/store-stores/apmis-new-store/apmis-new-store.component.ts
+++ src/app/system-modules/module-menu/apmis-store/store-stores/apmis-new-store/apmis-new-store.component.ts
@@ -3,8 +3,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FacilitiesService } from './../../../../../services/facility-manager/setup/facility.service';
 import { LocationService } from './../../../../../services/module-manager/setup/location.service';
 import { ProductTypeService } from './../../../../../services/facility-manager/setup/product-type.service';
+import { StoreService } from './../../../../../services/facility-manager/setup/store.service';
+import { ApmisFilterBadgeService } from './../../../../../services/tools/apmis-filter-badge.service';
 import { Facility } from 'app/models';
 import { CoolLocalStorage } from 'angular2-cool-storage';
+import { SystemModuleService } from 'app/services/module-manager/setup/system-module.service';
 
 @Component({
   selector: 'app-apmis-new-store',
@@ -20,39 +23,36 @@ export class ApmisNewStoreComponent implements OnInit {
   locations: any = [];
   storeLocations: any = [];
   productTypes: any = [];
+  placeholder = "Add Product Type";
 
   constructor(private fb: FormBuilder,
     private _locker: CoolLocalStorage,
     private facilitiesService: FacilitiesService,
     private locationService: LocationService,
-    private productTypeService: ProductTypeService) { }
+    private productTypeService: ProductTypeService,
+    private systemModuleService: SystemModuleService,
+    private storeService: StoreService,
+    private apmisFilterBadgeService: ApmisFilterBadgeService) { }
 
   ngOnInit() {
     this.newStoreForm = this.fb.group({
       'majorLoc': [' ', Validators.required],
       'minorLocationId': [' ', Validators.required],
       'name': [' ', Validators.required],
-      'productType': [' ', Validators.required],
+      'productTypeId': [' ', Validators.required],
       'description': [' ', Validators.required],
-      'canDespense': [' ', Validators.required],
-      'canReceivePurchaseOrder': [' ', Validators.required]
+      'canDespense': [true, Validators.required],
+      'canReceivePurchaseOrder': [true, Validators.required]
     });
-
-    // facilityId: { type: Schema.Types.ObjectId, require: true },
-    // name: { type: String, required: true },
-    // description: { type: String, required: false },
-    // store:[mStore],
-    // minorLocationId:{ type: Schema.Types.ObjectId, require: true },
-    // productTypeId:[productTypeIdModel],
-    // canDespense:{ type: Boolean, 'default': false },
-    // canReceivePurchaseOrder:{ type: Boolean, 'default': false }
-
 
     this.selectedFacility = <Facility>this._locker.getObject('selectedFacility');
     this.getFacilityService();
     this.getLocationService();
+    this.getProductTypeService();
     this.newStoreForm.controls['majorLoc'].valueChanges.subscribe(value => {
-      this.storeLocations = this.minorLocations.filter(x => x.locationId.toString() === value.toString());
+      if (value !== null && value !== '') {
+        this.storeLocations = this.minorLocations.filter(x => x.locationId.toString() === value.toString());
+      }
     });
   }
 
@@ -62,9 +62,22 @@ export class ApmisNewStoreComponent implements OnInit {
     }
   }
 
+  onDespense(event) {
+    this.newStoreForm.controls['canDespense'].setValue(event.target.checked);
+  }
+
+  onReceivePurchaseOrder(event) {
+    this.newStoreForm.controls['canReceivePurchaseOrder'].setValue(event.target.checked);
+  }
+
   getProductTypeService() {
-    this.productTypeService.find({ query: { facilityId: this.selectedFacility._id } }).then(payload => {
-      this.productTypes = payload.data;
+    this.productTypeService.find({ query: { facilityId: this.selectedFacility._id, isActive: true, $select: ['name'] } }).then(payload => {
+      this.productTypes = payload.data.map(x => {
+        return {
+          id: x._id,
+          label: x.name
+        };
+      });
     });
   }
 
@@ -78,6 +91,85 @@ export class ApmisNewStoreComponent implements OnInit {
     this.locationService.find({}).then(payload => {
       this.locations = payload.data;
     }, err => { })
+  }
+
+  onSearchSelectedItems(value) {
+    let productTypeItems = [];
+    value.forEach(element => {
+      productTypeItems.push({
+        productTypeId: element.id
+      });
+    });
+    this.newStoreForm.controls['productTypeId'].setValue(productTypeItems);
+  }
+
+  onCreateNewItem(value) {
+    const productType = {
+      facilityId: this.selectedFacility._id,
+      name: value
+    }
+    this.systemModuleService.on();
+    this.productTypeService.create(productType).then(payload => {
+      this.systemModuleService.off();
+      this.systemModuleService.announceSweetProxy('You have succesfully added ' + value.toString().toUpperCase() + ' as a Product type', 'success', null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null);
+      this.getProductTypeService();
+    }, err => {
+      this.systemModuleService.off();
+      this.systemModuleService.announceSweetProxy('Failed to add ' + value.toString().toUpperCase() + ' as a Product type', 'error', null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null);
+    });
+  }
+
+  onSave() {
+    this.systemModuleService.on();
+    if (this.newStoreForm.valid) {
+      const store = this.newStoreForm.value;
+      store.facilityId = this.selectedFacility._id;
+      this.storeService.create(store).then(payload => {
+        this.systemModuleService.announceSweetProxy('Store created successfully', 'success', null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null);
+        this.newStoreForm.reset();
+        this.systemModuleService.off();
+        this.apmisFilterBadgeService.reset(true);
+      }, err => {
+        this.systemModuleService.announceSweetProxy('Failed to create store', 'error', null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null);
+        this.systemModuleService.off();
+        this.newStoreForm.reset();
+        this.apmisFilterBadgeService.reset(true);
+      });
+
+    } else {
+      this.systemModuleService.announceSweetProxy('Required value is missing', 'error', null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null);
+      this.systemModuleService.off();
+    }
   }
 
 }
