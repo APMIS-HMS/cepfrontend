c98130431a157294ce61b92091f9b64d7bb96f22
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-12-07 22:29:03 +0100

    updates

--- src/app/system-modules/module-menu/apmis-store/product-entry/purchase-order/new-purchase-order-list/new-purchase-order-list.component.ts
+++ src/app/system-modules/module-menu/apmis-store/product-entry/purchase-order/new-purchase-order-list/new-purchase-order-list.component.ts
@@ -1,29 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { StoreGlobalUtilService } from '../../../store-utils/global-service';
 import { Filters } from '../../../store-utils/global';
-
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-purchase-order-list',
-  templateUrl: './new-purchase-order-list.component.html',
-  styleUrls: ['./new-purchase-order-list.component.scss']
+	selector: 'app-new-purchase-order-list',
+	templateUrl: './new-purchase-order-list.component.html',
+	styleUrls: [ './new-purchase-order-list.component.scss' ]
 })
 export class NewPurchaseOrderListComponent implements OnInit {
+	storeFilters = [];
+	selectedFilterIndex = 0;
+	filterType = '';
+	currentDate: FormControl;
+	constructor(private storeUtilService: StoreGlobalUtilService) {}
 
+	ngOnInit() {
+		this.currentDate = new FormControl(new Date().toISOString().substring(0, 10));
+		this.storeFilters = this.storeUtilService.getObjectKeys(Filters);
+	}
 
-  storeFilters = [];
-  selectedFilterIndex = 0;
-  filterType = '';
-
-  constructor(private storeUtilService: StoreGlobalUtilService) { }
-
-  ngOnInit() {
-    this.storeFilters = this.storeUtilService.getObjectKeys(Filters);
-  }
-
-  setSelectedFilter(index, filter) {
-    this.selectedFilterIndex = index;
-    this.filterType = filter;
-    //console.log(this.filterText);
-  }
+	setSelectedFilter(index, filter) {
+		this.selectedFilterIndex = index;
+		this.filterType = filter;
+		//console.log(this.filterText);
+	}
 }
