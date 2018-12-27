0aee1d1d3fa6fa10d811e7144ed33199686a03b6
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-12-27 11:34:09 +0100

    Updates to new prescription

--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/drug-search/drug-search.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/drug-search/drug-search.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-drug-search',
-  templateUrl: './drug-search.component.html',
-  styleUrls: ['./drug-search.component.scss']
+	selector: 'app-drug-search',
+	templateUrl: './drug-search.component.html',
+	styleUrls: [ './drug-search.component.scss' ]
 })
 export class DrugSearchComponent implements OnInit {
+	@Input()
+	products: any = [
+		{ name: 'Rimoxallin 400 MG Oral Capsule', marked: false },
+		{ name: 'Trimox 50 MG/ML Oral Suspension', marked: false },
+		{ name: 'Noltam 20 MG Oral Tablet', marked: false },
+		{ name: 'Trimox 250 MG Oral Capsule [Trimox]', marked: false }
+	];
+	@Input()
+	commonProducts: any = [
+		{ name: 'Amoxicillin 500 MG Oral Capsule [Rimoxallin]' },
+		{ name: 'Amoxicillin 50 MG/ML Oral Suspension [Trimox]' },
+		{ name: 'Tamoxifen 10 MG Oral Tablet [Noltam]' },
+		{ name: 'Amoxicillin 250 MG Oral Capsule [Trimox]' }
+	];
+	@Output() closeSearch: EventEmitter<any> = new EventEmitter<any>();
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	setMarked(event, product) {
+		// console.log(product);
+		// console.log(event.target.checked);
+	}
 
+	selectProduct(event, product) {
+		// console.log(product);
+		this.closeSearch.emit(product);
+	}
 }
--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/prescribe-drug/prescribe-drug.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/prescribe-drug/prescribe-drug.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-prescribe-drug',
-  templateUrl: './prescribe-drug.component.html',
-  styleUrls: ['./prescribe-drug.component.scss']
+	selector: 'app-prescribe-drug',
+	templateUrl: './prescribe-drug.component.html',
+	styleUrls: [ './prescribe-drug.component.scss' ]
 })
 export class PrescribeDrugComponent implements OnInit {
+	drugSearch = false;
+	constructor() {}
+	ngOnInit() {}
 
-  drugSearch = false;
-  constructor() { }
-  ngOnInit() {
-  }
-
-  onFocus(focus) {
-    if (focus === 'in') {
-      this.drugSearch = true;
+	onFocus(focus) {
+		if (focus === 'in') {
+			this.drugSearch = true;
 		} else {
-			setTimeout(() => {
-        this.drugSearch = false;
-			}, 500);
-		}    
+			// setTimeout(() => {
+			//   this.drugSearch = false;
+			// }, 500);
+		}
 	}
-
+	close_search(event) {
+		this.drugSearch = false;
+		console.log(event);
+	}
 }
