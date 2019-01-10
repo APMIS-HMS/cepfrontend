6d93296a9d0994d914e82e782160fc1b0bde8edc
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2019-01-10 20:52:08 +0100

    updates

--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/prescription-bill/gen-bill-search/gen-bill-search.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/prescription-bill/gen-bill-search/gen-bill-search.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { InventoryService } from 'app/services/facility-manager/setup';
+import { CoolLocalStorage } from 'angular2-cool-storage';
+import { Facility } from 'app/models';
 
 @Component({
-  selector: 'app-gen-bill-search',
-  templateUrl: './gen-bill-search.component.html',
-  styleUrls: ['./gen-bill-search.component.scss']
+	selector: 'app-gen-bill-search',
+	templateUrl: './gen-bill-search.component.html',
+	styleUrls: [ './gen-bill-search.component.scss' ]
 })
 export class GenBillSearchComponent implements OnInit {
+	@Input() drugs = [];
+	@Input() stores = [];
+	@Input() selectedDrug;
+	@Output() searchResultEvent: EventEmitter<any> = new EventEmitter<any>();
+	selectedFacility: any;
+	selectedStore: any;
+	drugPicked: any;
+	constructor(private _inventoryService: InventoryService, private _locker: CoolLocalStorage) {}
 
-  constructor() { }
+	ngOnInit() {
+		this.selectedFacility = <Facility>this._locker.getObject('selectedFacility');
+	}
 
-  ngOnInit() {
-  }
+	getDrugList() {
+		this.drugs = [];
+		this._inventoryService
+			.findList({
+				query: {
+					code: this.selectedDrug.code,
+					facilityId: this.selectedFacility._id,
+					storeId: this.selectedStore.storeId
+				}
+			})
+			.then((res) => {
+				this.drugs = res.data;
+				this.searchResultEvent.emit({ drugs: this.drugs, selectedStore: this.selectedStore });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+	selectStore(store) {
+		this.selectedStore = store;
+		this.getDrugList();
+	}
 
+	onItemChange(drug) {
+		this.drugPicked = drug;
+		console.log(this.drugPicked);
+	}
 }
--- src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/prescription-bill/prescription-bill.component.ts
+++ src/app/system-modules/module-menu/patient-manager/patientmanager-detailpage/new-patient-prescription/prescription-bill/prescription-bill.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { InventoryService } from 'app/services/facility-manager/setup';
+import { InventoryService, StoreService } from 'app/services/facility-manager/setup';
+import { FormControl } from '@angular/forms';
+import { Facility } from 'app/models';
+import { CoolLocalStorage } from 'angular2-cool-storage';
 
 @Component({
 	selector: 'app-prescription-bill',
@@ -15,18 +18,98 @@ export class PrescriptionBillComponent implements OnInit {
 	searchShow = false;
 	searchList = true;
 	drugs: any[] = [];
+	productSearchFormControl: FormControl = new FormControl();
+	selectedFacility: any;
+	stores: any;
+	selectedStore: any;
+	searchResults: any;
 
-	constructor(private _inventoryService: InventoryService) {}
+	constructor(
+		private _inventoryService: InventoryService,
+		private _storeService: StoreService,
+		private _locker: CoolLocalStorage
+	) {}
 
 	ngOnInit() {
-		this.getDrugList();
+		this.selectedFacility = <Facility>this._locker.getObject('selectedFacility');
+		this.getStores(this.selectedFacility._id);
+		// this.getDrugList();
+
+		this.productSearchFormControl.valueChanges.debounceTime(400).distinctUntilChanged().subscribe((value) => {
+			console.log(value);
+			this._inventoryService
+				.findFacilityProductList({
+					query: {
+						searchText: value,
+						facilityId: this.selectedFacility._id,
+						storeId: this.selectedStore.storeId,
+						direct: true
+					}
+				})
+				.then(
+					(payload) => {
+						this.searchResults = payload.data;
+						console.log(this.searchResults);
+					},
+					(error) => {
+						console.log(error);
+					}
+				);
+			// this.searchHasBeenDone = false;
+			// const searchList = this.canSearchBeDone(value.split(','));
+			// if (searchList.length > 1 && this.selectedProductName.length === 0) {
+			// 	this.selectedProductName = '';
+			// 	this._drugListApiService
+			// 		.find({
+			// 			query: {
+			// 				searchtext: searchList,
+			// 				po: false,
+			// 				brandonly: false,
+			// 				genericonly: true,
+			// 				$limit: false
+			// 			}
+			// 		})
+			// 		.then(
+			// 			(payload) => {
+			// 				this.products = this.modifyProducts(payload.data);
+			// 				if (this.products.length === 0) {
+			// 					this.searchHasBeenDone = true;
+			// 				} else {
+			// 					this.searchHasBeenDone = false;
+			// 				}
+			// 			},
+			// 			(error) => {
+			// 				this.searchHasBeenDone = false;
+			// 			}
+			// 		);
+			// } else {
+			// 	this.selectedProductName = '';
+			// }
+		});
+	}
+
+	getStores(facilityId, minorLocationId?) {
+		this._storeService
+			.getStoreList({
+				query: {
+					facilityId: facilityId,
+					minorLocationId: minorLocationId
+				}
+			})
+			.then(
+				(payload) => {
+					this.stores = payload.data.data;
+					console.log(this.stores);
+				},
+				(error) => {}
+			);
 	}
 
 	getDrugList() {
 		// query: { facilityId: this.facility._id, name: this.title, storeId: this.storeId }
 		this._inventoryService
 			.findList({
-				query: { code: this.drug.code }
+				query: { code: this.drug.code, facilityId: this.selectedFacility._id }
 			})
 			.then((res) => {
 				console.log(res);
@@ -41,12 +124,20 @@ export class PrescriptionBillComponent implements OnInit {
 		this.closeModal.emit(true);
 	}
 
-	onShowSearchResult() {
+	onShowSearchResult(product) {
 		this.searchShow = true;
 		this.searchList = false;
+		this.drugs.push(product);
 	}
 	onClickPrd() {
 		this.productAvailable = !this.productAvailable;
 		this.searchProduct = true;
+	}
+
+	receive_search_result_event(payload) {
+		if (payload.drugs.length === 0) {
+			this.onClickPrd();
+		}
+		this.selectedStore = payload.selectedStore;
 	}
 }
